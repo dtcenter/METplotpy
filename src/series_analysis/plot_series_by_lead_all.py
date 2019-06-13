48a4ef8b60b0fe7a419670be9bf21cecd49af43f
Minna Win <minnawin@ucar.edu>
2019-06-13 23:22:41 +0000

    Modified to plot *all* the netcdf files in each of the series_Fnnn subdirectories.

--- src/series_analysis/plot_series_by_lead_all.py
+++ src/series_analysis/plot_series_by_lead_all.py
@@ -1,22 +1,26 @@
 import os
+import errno
 from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 from cartopy.util import add_cyclic_point
 from matplotlib import cm
+from collections import namedtuple
+import re
 
-def generate_plot(input_nc_file_dir, input_nc_filename, variable_name, level, output_filename,
-                background_on=False):
+
+def generate_plot(input_nc_file_dir, input_nc_filename, variable_name, level_type, level, output_filename,
+                  background_on=False):
     '''
 
-    :param input_file:
-    :param input_nc_filename
-    :param variable_name
+    :param input_file:  netcdf input directory
+    :param input_nc_filename  netcdf input filename
+    :param variable_name  name of the variable from the netcdf file
     :param output_filename
     :return:
     '''
 
-    #read in the netcdf file
+    # read in the netcdf file
     try:
         input_nc_file = os.path.join(input_nc_file_dir, input_nc_filename)
         file_handle = Dataset(input_nc_file, mode='r')
@@ -33,11 +37,11 @@ def generate_plot(input_nc_file_dir, input_nc_filename, variable_name, level, ou
         # close the file handle now that we are finished retrieving what we need
         file_handle.close()
 
-        # Verify that these values are consistent with lat, lon values, etc.
-        print("lat : ", lats)
-        print("lon : ", lons)
-        print("FBAR: ", fbar)
-        print("OBAR: ", obar)
+        # Uncomment to visually verify that these values are "reasonable" lat/lon values, etc.
+        # print("lat : ", lats)
+        # print("lon : ", lons)
+        # print("FBAR: ", fbar)
+        # print("OBAR: ", obar)
 
         # Use the reversed Spectral colormap to reflect temperatures.
         cmap = cm.get_cmap('Spectral_r')
@@ -79,7 +83,7 @@ def generate_plot(input_nc_file_dir, input_nc_filename, variable_name, level, ou
             sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(minimum, maximum))
             sm._A = []
             plt.colorbar(sm, ax=geo_ax)
-            title = var_in_title + " from series by init for " + variable_name + " " + level
+            title = var_in_title + " from series by init for " + variable_name + " " + level_type + level
             plt.title(title)
 
             # output file will be saved as png
@@ -90,11 +94,79 @@ def generate_plot(input_nc_file_dir, input_nc_filename, variable_name, level, ou
             print("output filename: ", output_png_file)
             plt.savefig(output_png_file)
 
+
+def get_info(base_dir, output_base_dir):
+    '''From the base_dir, where the series_F### subdirectories reside (and contain the netcdf output from the
+       feature relative use case):
+       1) get all the subdirectories
+       2) from each subdirectory, get all the netcdf files
+       3) from the netcdf file, extract the fhr, variable name, and level information
+       4) Create the output filename, variable name, level, forecast hour to be used to create the title for the plot.
+
+       :param base_dir  The base directory containing the series_F### subdirectories, which in turn contain the netcdf
+                        files created by running the METplus use cases
+       :param output_base_dir The base directory where the plots are stored
+
+       :return: file_info_list   A list of named tuples that contain the file info needed to generate the title of
+                                 each plot and the output filename of each plot.
+    '''
+
+    # Get a list of all the subdirectories under the base_dir and create a list of named tuples that contain the
+    # forecast hour, variable (TMP or HGT), level type (ie P or Z) and level.
+    FileInfo = namedtuple('FileInfo', 'fhr, variable,level_type, level, output_filename')
+
+    # filename looks like the following: series_[fhr]_[variable]_[level].nc
+    FileInfo = namedtuple('FileInfo', 'fhr, variable_name, level_type, level, output_filename')
+    file_info_list = []
+
+    for root, dirs, files in os.walk(base_dir):
+        for file in files:
+            match = re.search('series_F([0-9]{3})_(HGT|TMP)_(P|Z)([0-9]{3}).nc', file)
+            if match:
+                fhr = match.group(1)
+                variable_name = match.group(2)
+                level_type = match.group(3)
+                level = match.group(4)
+                # Create the output filename (full path) but omit the extension
+                output_filename = 'series_F' + fhr + "_" + variable_name + '_' + level_type + level
+                output_file_no_ext = os.path.join(output_base_dir, output_filename)
+                cur_file_info = FileInfo(fhr, variable_name, level_type, level, output_file_no_ext)
+                file_info_list.append(cur_file_info)
+
+    return file_info_list
+
+
 if __name__ == "__main__":
-    input_dir = "/d1/METplus_Plotting_Data/series_by_lead_all_fhrs/series_F000"
+
+    # Generate plots for all the netcdf files found in the /d1/METplus_Plotting_Data/series_by_lead_all_fhrs:
+    # series_F000/, series_F006/, series_F012, and series_F018
+    base_dir = "/d1/METplus_Plotting_Data/series_by_lead_all_fhrs"
+    input_dir_base = "/d1/METplus_Plotting_Data/series_by_lead_all_fhrs"
     input_file = "series_F000_TMP_P850.nc"
-    output_filename = '/d1/METplus_Plotting_Data/series_by_lead_all_fhrs/series_F000/series_by_lead'
-    variable_name = 'TMP'
-    level = 'P850'
+    output_base_dir = '/d1/METplus_Plotting_Data/series_by_lead_all_fhrs/output'
+
+
+    # create the directory if it doesn't exist (equivalent of mkdir -p)
+    try:
+        os.makedirs(output_base_dir)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(output_base_dir):
+            pass
+
+    # Default is false, set this to True only if you want to see coastlines drawn
     plot_background_map = True
-    generate_plot(input_dir, input_file, variable_name, level, output_filename, plot_background_map )
+
+    file_info_list = get_info(base_dir, output_base_dir)
+    for file_info in file_info_list:
+        hr = file_info.fhr
+        fhr = 'series_F' + hr
+        input_dir = os.path.join(input_dir_base, fhr)
+        level_type = file_info.level_type
+        level = file_info.level
+        variable_name = file_info.variable_name
+        input_filename = 'series_F' + hr + '_' + variable_name + '_' + level_type + \
+                         level + '.nc'
+        input_file = os.path.join(input_dir, input_filename)
+        output_filename = file_info.output_filename
+
+        generate_plot(input_dir, input_file, variable_name, level_type, level, output_filename, plot_background_map )
