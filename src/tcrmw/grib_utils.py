6615c5c072b25b8e05c04b5d1cb7a00685fa30fe
davidfillmore <fillmore.winslow.david@gmail.com>
2019-06-13 14:31:09 -0600

    Switching to xarray/cfgrib in grib_utils.py.

--- src/tcrmw/grib_utils.py
+++ src/tcrmw/grib_utils.py
@@ -2,17 +2,20 @@ import os
 import sys
 import logging
 import numpy as np
-import pygrib
+import xarray as xr
 
 def read_grib_times(datadir, filelist):
     """
     Read times from a list of grib files.
     """
     for filename in filelist:
-        filename = os.path.join(datadir, filename)
+        filename = os.path.join(datadir, filename.rstrip())
         try:
-            file_id = pygrib.open(filename)
+            ds = xr.open_dataset(filename, engine='cfgrib',
+                backend_kwargs={
+                'filter_by_keys' : {'typeOfLevel' : 'atmosphere'}})
             logging.info('reading ' + filename)
+            logging.info(ds)
         except IOError:
             logging.error('failed to open ' + filename)
             sys.exit()
