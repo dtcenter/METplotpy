626df7bcbd756ac4b88c693566bedc79aa8f76df
Minna Win <minnawin@ucar.edu>
2019-06-24 21:38:01 +0000

    Cleaned up unused variables.

--- src/series_analysis/animate_series_by_lead_all.py
+++ src/series_analysis/animate_series_by_lead_all.py
@@ -15,27 +15,23 @@ def animate_series(input_dir):
     '''
 
 
-def collect_files_to_animate(input_dir, filename_regex, forecast_hours, variable, level_type, level, statistic,
-                             animation_duration, is_png=True):
+def collect_files_to_animate(input_dir, forecast_hours, variable, level_type, level, statistic, is_png=True):
     ''' Collect a list of png/jpg files (the only two formats that are supported by the imageio module)
         that have the same var, level, and stat to include in the gif file.  Files
         will all have the following format: series_Fnnn_[var]_[level]_[stat].png, where
            :param input_dir: The directory where the png/jpg files to be animated are located
-           :param filename_regex: The regex describing the format of the input png/jpg file to be included in the
-                  animation
            :param forecast_hours:  the list of forecast hours comprising the time frame of the animation
            :param var is the variable: TMP, HGT, etc.
            :param level_type: Level type as indicated by P for pressure level, Z for height above ground, etc
            :param level is the level: 500, 850, etc.
            :param statistic: statistic of interests, either fbar or obar
-           :param animation_duration: The amount of time spent on each "frame" of the animation, in seconds.
            :param is_png: flag to indicate if this is a png file or jpg file, by default this is set to True, if
                           files to be animated are jpg, set to False
            Two gif files will be created, one for the specified fhrs, variable, and level for obar statistic
            and the other for the fbar statistic.
 
-           :returns sorted_var_level_stat_paths: a list of png or jpg files based on fhr, variable, level type, level, and statistic,
-                         sorted by ascending order
+           :returns sorted_var_level_stat_paths: a list of png or jpg files based on fhr, variable, level type,
+                                                 level, and statistic sorted by ascending order
 
     '''
 
@@ -69,7 +65,6 @@ def create_output_filename(output_dir, file_to_animate, filename_regex):
     '''
 
     # filename_regex = "series_F([0-9]{3})_([A-Z]{3})_((P|Z)[0-9]{1,3})_(obar|fbar).png"
-    print("file to animate: ", file_to_animate)
     match = re.match(filename_regex, file_to_animate)
     if match:
         variable = match.group(2)
