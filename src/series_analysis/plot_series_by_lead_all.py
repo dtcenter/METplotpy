ff23cd90a9c1dd22a268659f94385bf6f0479968
Minna Win <minnawin@ucar.edu>
2019-07-01 17:11:57 +0000

    Cleaned up code after running pylint linter.  Ignore some warnings that are not really issues, like import warning with netCDF4, some unused variables (that occur when walking the os tree), etc.

--- src/series_analysis/plot_series_by_lead_all.py
+++ src/series_analysis/plot_series_by_lead_all.py
@@ -1,15 +1,24 @@
+
+"""
+This module/script generates static plots (png) from netcdf
+output from performing a series by lead for all fhrs.  This
+is the output from the METplus feature relative use case.
+"""
+
 import os
 import errno
-from netCDF4 import Dataset
-import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-from cartopy.util import add_cyclic_point
-from matplotlib import cm
 from collections import namedtuple
 import re
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import cartopy.crs as ccrs
+from cartopy.util import add_cyclic_point
+#pylint: disable=import-error
+from netCDF4 import Dataset
 
 
-def generate_plot(input_nc_file_dir, input_nc_filename, fhr,  variable_name, level_type, level, output_filename,
+def generate_plot(input_nc_file_dir, input_nc_filename, fhr, variable_name,
+                  level_type, level, output_filename,
                   background_on=False):
     '''
 
@@ -17,7 +26,8 @@ def generate_plot(input_nc_file_dir, input_nc_filename, fhr,  variable_name, lev
     :param input_nc_filename  netcdf input filename
     :param fhr  The forecast hour in format Fhhh
     :param variable_name  name of the variable from the netcdf file
-    :param level_type :  the letter indicating the type of level (ie P for pressure level, Z for height above ground)
+    :param level_type :  the letter indicating the type of level
+                        (ie P for pressure level, Z for height above ground)
     :param output_filename:  The full filepath of the png output file
     :return:
     '''
@@ -63,29 +73,37 @@ def generate_plot(input_nc_file_dir, input_nc_filename, fhr,  variable_name, lev
 
             if key == 'OBAR':
                 var_in_title = "OBAR"
-                # Figure out the minimum and maximum values of the OBAR/FBAR temperature and use these values in the
-                # plt.Normalize() call.
+                # Figure out the minimum and maximum values of the OBAR/FBAR temperature
+                # and use these values in the plt.Normalize() call.
                 minimum = obar.min()
                 maximum = obar.max()
-                # Allow the temperature values (OBAR, FBAR) and the longitude to cycle (encircle the globe)
+                # Allow the temperature values (OBAR, FBAR) and the longitude to cycle
+                # (encircle the globe)
+                #pylint: disable=unused-variable
                 obar_cyc, lon_cyc = add_cyclic_point(obar, coord=lons)
             else:
                 var_in_title = "FBAR"
-                # Figure out the minimum and maximum values of the OBAR/FBAR temperature and use these values in the
-                # plt.Normalize() call.
+                # Figure out the minimum and maximum values of the OBAR/FBAR temperature
+                # and use these values in the plt.Normalize() call.
                 minimum = fbar.min()
                 maximum = fbar.max()
-                # Allow the temperature values (OBAR, FBAR) and the longitude to cycle (encircle the globe)
+                # Allow the temperature values (OBAR, FBAR) and the longitude to
+                # cycle (encircle the globe)
+                # pylint: disable=unused-variable
                 fbar_cyc, lon_cyc = add_cyclic_point(fbar, coord=lons)
 
-            # Adding a legend from Stack Overflow.  Create your own mappable object (scalar mappable) that can be
-            # passed to colorbar.  Normalize values are the min and max values normalized to 0, 1 in the
-            # solution, here we use the minimum and maximum values found in the FBAR/OBAR array.
+            # Adding a legend from Stack Overflow.  Create your own mappable
+            # object (scalar mappable) that can be passed to colorbar.
+            # Normalize values are the min and max values normalized to
+            # 0, 1 in the solution, here we use the minimum and maximum
+            # values found in the FBAR/OBAR array.
 
-            sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(minimum, maximum))
-            sm._A = []
-            plt.colorbar(sm, ax=geo_ax)
-            title = " series by init " + var_in_title + " for fhr " + fhr + " " + variable_name + " " + level_type + level
+            scalar_mappable = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(minimum, maximum))
+            #pylint: disable=protected-access
+            scalar_mappable._A = []
+            plt.colorbar(scalar_mappable, ax=geo_ax)
+            title = " series by init " + var_in_title + " for fhr " + fhr + " " + \
+                    variable_name + " " + level_type + level
             plt.title(title)
 
             # output file will be saved as png
@@ -98,29 +116,35 @@ def generate_plot(input_nc_file_dir, input_nc_filename, fhr,  variable_name, lev
 
 
 def get_info(base_dir, output_base_dir):
-    '''From the base_dir, where the series_F### subdirectories reside (and contain the netcdf output from the
+    '''From the base_dir, where the series_F### subdirectories reside (and contain the
+    netcdf output from the
        feature relative use case):
        1) get all the subdirectories
        2) from each subdirectory, get all the netcdf files
        3) from the netcdf file, extract the fhr, variable name, and level information
-       4) Create the output filename, variable name, level, forecast hour to be used to create the title for the plot.
+       4) Create the output filename, variable name, level, forecast hour to be used
+       to create the title for the plot.
 
-       :param base_dir  The base directory containing the series_F### subdirectories, which in turn contain the netcdf
+       :param base_dir  The base directory containing the series_F### subdirectories,
+                        which in turn contain the netcdf
                         files created by running the METplus use cases
        :param output_base_dir The base directory where the plots are stored
 
-       :return: file_info_list   A list of named tuples that contain the file info needed to generate the title of
+       :return: file_info_list   A list of named tuples that contain the file info
+                                 needed to generate the title of
                                  each plot and the output filename of each plot.
     '''
 
-    # Get a list of all the subdirectories under the base_dir and create a list of named tuples that contain the
-    # forecast hour, variable (TMP or HGT), level type (ie P or Z) and level.
+    # Get a list of all the subdirectories under the base_dir and create a list of named
+    # tuples that contain the forecast hour, variable (TMP or HGT), level type
+    # (ie P or Z) and level.
     FileInfo = namedtuple('FileInfo', 'fhr, variable,level_type, level, output_filename')
 
     # filename looks like the following: series_[fhr]_[variable]_[level].nc
     FileInfo = namedtuple('FileInfo', 'fhr, variable_name, level_type, level, output_filename')
     file_info_list = []
 
+    # pylint: disable=unused-variable
     for root, dirs, files in os.walk(base_dir):
         for file in files:
             match = re.search('series_F([0-9]{3})_(HGT|TMP)_(P|Z)([0-9]{3}).nc', file)
@@ -138,14 +162,13 @@ def get_info(base_dir, output_base_dir):
     return file_info_list
 
 
-if __name__ == "__main__":
-
-    # Generate plots for all the netcdf files found in the /d1/METplus_Plotting_Data/series_by_lead_all_fhrs:
-    # series_F000/, series_F006/, series_F012, and series_F018
+def main():
+    """Generate plots for all the netcdf files found in the
+    /d1/METplus_Plotting_Data/series_by_lead_all_fhrs:
+    series_F000/, series_F006/, series_F012, and series_F018
+    """
     input_dir_base = "/d1/METplus_Plotting_Data/series_by_lead_all_fhrs"
-    input_file = "series_F000_TMP_P850.nc"
     output_base_dir = '/d1/METplus_Plotting_Data/series_by_lead_all_fhrs/output'
-
 
     # create the directory if it doesn't exist (equivalent of mkdir -p)
     try:
@@ -159,15 +182,21 @@ if __name__ == "__main__":
 
     file_info_list = get_info(input_dir_base, output_base_dir)
     for file_info in file_info_list:
-        hr = file_info.fhr
-        fhr = 'series_F' + hr
+        hour = file_info.fhr
+        fhr = 'series_F' + hour
         input_dir = os.path.join(input_dir_base, fhr)
         level_type = file_info.level_type
         level = file_info.level
         variable_name = file_info.variable_name
-        input_filename = 'series_F' + hr + '_' + variable_name + '_' + level_type + \
+        input_filename = 'series_F' + hour + '_' + variable_name + '_' + level_type + \
                          level + '.nc'
         input_file = os.path.join(input_dir, input_filename)
         output_filename = file_info.output_filename
 
-        generate_plot(input_dir, input_file, hr, variable_name, level_type, level, output_filename, plot_background_map )
+        generate_plot(input_dir, input_file, hour, variable_name, level_type, level,
+                      output_filename, plot_background_map)
+
+
+
+if __name__ == "__main__":
+    main()
