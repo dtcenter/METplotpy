c51f1d31fa289473857de490c42ec608eae0b9c9
Minna Win <minnawin@ucar.edu>
2019-07-01 16:25:53 +0000

    Modifications made after running pylint linter on code.  Score is now 9.69/10 with some warnings ignored.

--- src/grid_to_grid/plot_grid_to_grid.py
+++ src/grid_to_grid/plot_grid_to_grid.py
@@ -1,38 +1,48 @@
+"""
+Creates static graphics plots (png) reading in the netcdf files that are the output from running the
+grid-to-grid use case with the anom.conf file.
+"""
 import os
 import re
 import errno
-from netCDF4 import Dataset
+from matplotlib import cm
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 from cartopy.util import add_cyclic_point
-from matplotlib import cm
+#pylint: disable=import-error
+from netCDF4 import Dataset
 
 
-def create_plots(input_dir, variable_name, nc_var_name, title, nc_flag_type, output_dir, background_on=False):
-    '''
-    Creates plots of output from grid_to_grid METplus wrapper use case, where the nc_pair_flags raw and diff are
-    set to TRUE to generate the output for the raw variables for fcst and obs, and their corresponding difference.
+
+def create_plots(input_dir, variable_name, nc_var_name, title, nc_flag_type, output_dir,
+                 background_on=False):
+    """
+    Creates plots of output from grid_to_grid METplus wrapper use case,
+    where the nc_pair_flags raw and diff are
+    set to TRUE to generate the output for the raw variables for fcst and obs,
+    and their corresponding difference.
 
     :param input_dir:
     :param variable_name: The variable of interest: tmp, ugrd, hgt, etc
     :param nc_var_name: The variable name as described in the netcdf file
     :param title:  The title for this plot
-    :param nc_flag_type:  Corresponds *somewhat* to the nc_pairs_flag: FCST&OBS correspond to raw, DIFF corresponds to diff
+    :param nc_flag_type:  Corresponds *somewhat* to the nc_pairs_flag: FCST&OBS correspond to raw,
+                          DIFF corresponds to diff
     :param output_dir:  The directory where the png files will be saved.
     :return:
-    '''
+    """
 
     # Set the type to all upper case, and variable_names to lower case
 
     # Get all the netcdf output files in the input directory
     all_nc_filenames = []
 
+    #pylint: disable=unused-variable
     for root, dirs, files in os.walk(input_dir):
         for file in files:
             match = re.match(r'(.*).nc', file)
             if match:
                 all_nc_filenames.append(file)
-
 
     # For each netcdf file, open the file, and
     # retrieve the fields from the netcdf file
@@ -40,7 +50,7 @@ def create_plots(input_dir, variable_name, nc_var_name, title, nc_flag_type, out
         try:
             file_handle = Dataset(os.path.join(input_dir, nc_file), mode='r')
         except FileNotFoundError:
-            print("File ",nc_file, " does not exist.")
+            print("File ", nc_file, " does not exist.")
         else:
             # Retrieve variables of interest
             lons = file_handle.variables['lon'][:]
@@ -49,7 +59,6 @@ def create_plots(input_dir, variable_name, nc_var_name, title, nc_flag_type, out
 
             # Finished acquiring variables, no longer need the file_handle.
             file_handle.close()
-
 
         # print("netcdf file: ", nc_file)
         # print(nc_var_name, ': ', variable)
@@ -69,8 +78,8 @@ def create_plots(input_dir, variable_name, nc_var_name, title, nc_flag_type, out
         if background_on:
             geo_ax.coastlines()
 
-
-        # Figure out the minimum and maximum values of the OBAR/FBAR temperature and use these values in the
+        # Figure out the minimum and maximum values of the OBAR/FBAR temperature
+        # and use these values in the
         # plt.Normalize() call.
         minimum = variable.min()
         maximum = variable.max()
@@ -84,24 +93,32 @@ def create_plots(input_dir, variable_name, nc_var_name, title, nc_flag_type, out
         # set number of contour levels to 65, higher number results in more smoothing.
         plt.contourf(lons, lats, variable, 65, transform=ccrs.PlateCarree(), cmap=cmap)
 
-        # Adding a legend from Stack Overflow.  Create your own mappable object (scalar mappable) that can be
-        # passed to colorbar.  Normalize values are the min and max values normalized to 0, 1 in the
+        # Adding a legend from Stack Overflow.  Create your own mappable object
+        # (scalar mappable) that can be passed to colorbar.  Normalize values
+        # are the min and max values normalized to 0, 1 in the
         # solution, here we use the minimum and maximum values found in the FBAR/OBAR array.
 
-        sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(minimum, maximum))
-        sm._A = []
-        plt.colorbar(sm, ax=geo_ax)
+        scalar_mappable = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(minimum, maximum))
+        # pylint: disable=protected-access
+        scalar_mappable._A = []
+        plt.colorbar(scalar_mappable, ax=geo_ax)
         plt.title(title)
 
         # output file will be saved as png
-        output_png_file = variable_name + "_" + nc_flag_type + "_" + os.path.splitext(nc_file)[0] + ".png"
+        output_png_file = variable_name + "_" + nc_flag_type + "_" + \
+                          os.path.splitext(nc_file)[0] + ".png"
         print("output png file: ", output_png_file)
-        plt.savefig(os.path.join(output_dir,output_png_file))
+        plt.savefig(os.path.join(output_dir, output_png_file))
 
 
-if __name__ == "__main__":
+def main():
+    """
+    Set up the input variables needed to generate the plots
+    :return:
+    """
 
-    # generate the plots of all the raw fields and diffs for the grid_to_grid use case results generated by running
+    # generate the plots of all the raw fields and diffs for the grid_to_grid use
+    # case results generated by running
     # the anom.conf file.
 
     # The directory where the grid stat netcdf output files are located.
@@ -119,9 +136,11 @@ if __name__ == "__main__":
     # Indicate the fcst and obs raw fields of interest and the corresponding diff field
     # e.g. nc_fcst_var = FCST_TMP_P850_FULL
     #      nc_obs_var = OBS_TMP_P850_FULL
-    #      nc_diff_var = DIFF_TMP_P850_FULL    which are obtained by performing an 'ncdump' on the netcdf file and
+    #      nc_diff_var = DIFF_TMP_P850_FULL
+    #      which are obtained by performing an 'ncdump' on the netcdf file and
     #      and assigning the variable names exactly as they are displayed in the ncdump output.
-    # The 'var' is set to the variable name ie. TMP, UGRD, VGRD, HGT, etc...and is used as a prefix to the output file
+    # The 'var' is set to the variable name ie. TMP, UGRD, VGRD, HGT, etc...
+    # and is used as a prefix to the output file
     # name to differentiate it from the plots of other variables.
     var = 'TMP'
     nc_fcst_var = 'FCST_TMP_P850_FULL'
@@ -152,11 +171,20 @@ if __name__ == "__main__":
     # set to True if you want to draw the coastlines on the plot
     background_on = True
 
-    # nc_flag_type corresponds to the nc_flag in the MET grid-stat config file: ie  FCST or OBS corresponds to raw=TRUE,
+    # nc_flag_type corresponds to the nc_flag in the MET grid-stat config file:
+    # ie  FCST or OBS corresponds to raw=TRUE,
     # and DIFF corresponds to diff=TRUE
-    nc_flag_type="FCST"
-    create_plots(input_dir, var, nc_fcst_var, nc_fcst_title, nc_flag_type, output_dir, background_on)
-    nc_flag_type="OBS"
-    create_plots(input_dir, var, nc_obs_var, nc_obs_title, nc_flag_type, output_dir, background_on)
-    nc_flag_type="DIFF"
-    create_plots(input_dir, var, nc_diff_var, nc_diff_title, nc_flag_type, output_dir, background_on)
+    nc_flag_type = "FCST"
+    create_plots(input_dir, var, nc_fcst_var, nc_fcst_title, nc_flag_type,
+                 output_dir, background_on)
+    nc_flag_type = "OBS"
+    create_plots(input_dir, var, nc_obs_var, nc_obs_title, nc_flag_type,
+                 output_dir, background_on)
+    nc_flag_type = "DIFF"
+    create_plots(input_dir, var, nc_diff_var, nc_diff_title, nc_flag_type,
+                 output_dir, background_on)
+
+
+if __name__ == "__main__":
+
+    main()
