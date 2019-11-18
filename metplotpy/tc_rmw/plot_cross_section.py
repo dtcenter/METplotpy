869f3a07d81ffb62d685f9acd51258199cb5e06b
davidfillmore <fillmore.winslow.david@gmail.com>
2019-11-18 14:10:48 -0700

    Start on plot_cross_section for TC-RMW output.

--- metplotpy/tc_rmw/plot_cross_section.py
+++ metplotpy/tc_rmw/plot_cross_section.py
@@ -7,3 +7,23 @@ import matplotlib
 # matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+from tc_utils import read_tcrmw
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description='Plot Tropical Cyclone Cross Section')
+
+    parser.add_argument(
+        '--datadir', type=str, dest='datadir',
+        required=True)
+    parser.add_argument(
+        '--filename', type=str, dest='filename',
+        required=True)
+
+    args = parser.parse_args()
+
+    logging.basicConfig(stream=sys.stdout,
+        level=logging.DEBUG)
+
+    read_tcrmw(os.path.join(args.datadir, args.filename))
