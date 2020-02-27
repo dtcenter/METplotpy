ba15e8564961483090f444cdf628bedf4bb0aab7
bikegeek <minnawin@ucar.edu>
2020-02-27 14:13:04 -0700

    Files for generating Python distribution of plotting package implemented with Plotly.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="metplotpy",
+    version="0.0.1",
+    author="METplus",
+    author_email="met-help@ucar.edu",
+    description="plotting package for METplus",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NCAR/METplotpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License", 
+         "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
