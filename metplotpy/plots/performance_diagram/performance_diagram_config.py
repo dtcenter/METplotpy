8a0e0a59d50b289ff27a7f91f1e920f66c2af5ed
bikegeek <minnawin@ucar.edu>
2020-06-03 09:18:57 -0600

    Remove unused import

--- metplotpy/plots/performance_diagram/performance_diagram_config.py
+++ metplotpy/plots/performance_diagram/performance_diagram_config.py
@@ -8,7 +8,6 @@ __email__ = 'met_help@ucar.edu'
 
 import re
 from plots.config import Config
-from plots.met_plot import MetPlot
 
 class PerformanceDiagramConfig(Config):
     ACCEPTABLE_CI_VALS = ['NONE', 'BOOT', 'NORM']
