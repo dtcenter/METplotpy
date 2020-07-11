8b4de5481e596e6d4c6ddaaf3abaa5f33ae44bbf
bikegeek <minnawin@ucar.edu>
2020-07-10 18:44:55 -0600

    First version of plotly implementation.  Cannot support number of columns in legend labels, so either draw legend labels horizontally or vertically.  Warning about orca package path not found (in spite of it being included in the conda env).  Annotations for threshold values are being displayed.

--- metplotpy/plots/roc_diagram/roc_diagram.py
+++ metplotpy/plots/roc_diagram/roc_diagram.py
@@ -6,13 +6,19 @@ __email__ = 'met_help@ucar.edu'
 
 import os
 import yaml
+import re
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
+import plotly
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import constants
 
 import plots.util as util
 from plots.met_plot import MetPlot
 from roc_diagram_config import ROCDiagramConfig
 from roc_diagram_series import ROCDiagramSeries
+
 
 class ROCDiagram(MetPlot):
     """
@@ -55,8 +61,7 @@ class ROCDiagram(MetPlot):
         # Need to have a self.figure that we can pass along to
         # the methods in met_plot.py (MetPlot class methods) to
         # create binary versions of the plot.
-        # self.figure = self._create_figure()
-
+        self.figure = self._create_figure()
 
     def _read_input_data(self):
         """
@@ -71,7 +76,6 @@ class ROCDiagram(MetPlot):
 
         """
         return pd.read_csv(self.config_obj.stat_input, sep='\t', header='infer')
-
 
     def _create_series(self, input_data):
         """
@@ -101,7 +105,6 @@ class ROCDiagram(MetPlot):
             series_list.append(series_obj)
         return series_list
 
-
     def remove_file(self):
         """
            Removes previously made image file .  Invoked by the parent class before self.output_file
@@ -114,7 +117,6 @@ class ROCDiagram(MetPlot):
         if os.path.exists(image_name):
             os.remove(image_name)
 
-
     def _create_figure(self):
         """
         Generate the performance diagram of varying number of series with POD and 1-FAR
@@ -126,12 +128,194 @@ class ROCDiagram(MetPlot):
 
 
         Returns:
-            Generates a performance diagram with equal lines of CSI (Critical Success Index)
-            and equal lines of bias
+             ROC diagram
         """
 
         # This creates a figure of height and width as specified by the user
-        fig = plt.figure(figsize=[self.config_obj.plot_height, self.config_obj.plot_width])
+        # fig = plt.figure(figsize=[self.config_obj.plot_height, self.config_obj.plot_width],
+        #                  dpi=self.config_obj.plot_resolution)
+        #
+        # ax1 = fig.add_subplot()
+        # ax2 = ax1.twinx()
+        # ax1.set_ylim(0.0, 1.0)
+        # ax2.set_ylim(0.0, 1.0)
+        # ax1.set_xlim(0.0, 1.0)
+        # plt.title(self.config_obj.title, fontsize=constants.DEFAULT_TITLE_FONTSIZE,
+        #           color=constants.DEFAULT_TITLE_COLOR, fontweight="bold",
+        #           fontfamily=constants.DEFAULT_TITLE_FONT)
+        # ax1.set_xlabel(self.config_obj.xaxis)
+        # ax1.set_ylabel(self.config_obj.yaxis_1)
+        # ax2.set_ylabel(self.config_obj.yaxis_2)
+        #
+
+
+        fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+        # Add figure title
+        fig.update_layout(
+            title={'text': self.config_obj.title,
+                   'y': 0.95,
+                   'x': 0.5,
+                   'xanchor': "center",
+                   'yanchor': "top"},
+            plot_bgcolor="#FFF"
+
+        )
+        # Set x-axis title
+        fig.update_xaxes(title_text=self.config_obj.xaxis, linecolor="black", linewidth=2, showgrid=False,
+                         range=[0.0, 1.0])
+        fig.update_xaxes(title_text=self.config_obj.xaxis, linecolor="black", linewidth=2, showgrid=False,
+                         range=[0.0, 1.0])
+
+        # Set y-axes titles
+        # fig.update_yaxes(title_text="<b>primary</b> yaxis title", secondary_y=False)
+        fig.update_yaxes(title_text=self.config_obj.yaxis_1, secondary_y=False, linecolor="black", linewidth=2,
+                         showgrid=False, zeroline=False, range=[0.0,1.0])
+        fig.update_yaxes(title_text=self.config_obj.yaxis_2, secondary_y=True, linecolor="black", linewidth=2,
+                         showgrid=False, zeroline=False, range=[0.0,1.0])
+
+        # plot the no-skill line
+        x = [0., 1.]
+        y = [0., 1.]
+        fig.add_trace(go.Scatter(x=x, y=y, line=dict(color='grey',
+                                                     width=1.2,
+                                                     dash='dash'
+                                                     ),
+                                 name='no skill line',
+                                 showlegend=False
+                                 ))
+
+        # style the legend box
+        if self.config_obj.draw_box:
+            fig.update_layout(legend=dict(x=self.config_obj.bbox_x,
+                                          y=self.config_obj.bbox_y,
+                                          bordercolor="black",
+                                          borderwidth=2
+                                          ))
+
+        else:
+            fig.update_layout(legend=dict(x=self.config_obj.bbox_x,
+                                          y=self.config_obj.bbox_y
+                                          ))
+
+        # can't support number of columns in legend, can only choose
+        # between horizontal or vertical alignment of legend labels
+        if self.config_obj.legend_ncol > 1:
+            # horizontal (multiple columns)
+            fig.update_layout(legend=dict(x=self.config_obj.bbox_x,
+                                          y=self.config_obj.bbox_y,
+                                          bordercolor="black",
+                                          orientation="h",
+                                          borderwidth=2
+                                          ))
+        else:
+            # vertical (one column)
+            fig.update_layout(legend=dict(x=self.config_obj.bbox_x,
+                                          y=self.config_obj.bbox_y,
+                                          bordercolor="black",
+                                          borderwidth=2
+                                          ))
+        thresh_list = []
+
+
+        for idx, series in enumerate(self.series_list):
+            for i, thresh_val in enumerate(series.series_points[2]):
+                thresh_list.append(str(thresh_val))
+
+            # Don't generate the plot for this series if
+            # it isn't requested (as set in the config file)
+            if series.plot_disp:
+                pofd_points = series.series_points[0]
+                pody_points = series.series_points[1]
+                fig.add_trace(
+                    go.Scatter(x=pofd_points, y=pody_points, mode="lines+markers+text", showlegend=True,text=thresh_list, textposition="top right"),
+                    secondary_y=False
+                )
+
+
+                # for idx, thresh_val in enumerate(thresh):
+                #     plt.annotate(str(thresh_val),
+                #                  (pofd_points[idx], pody_points[idx]), fontsize=9)
+
+            # plt.plot(pofd_points, pody_points)
+            return fig
+
+    def abline(self, slope, intercept, axes):
+        """
+            R has abline(), matplotlib does not have this function.
+            This is a solution from StackOverflow...
+            Plot a line from slope and intercept
+        """
+        # axes = plt.gca()
+        x_vals = np.array(axes.get_xlim())
+        y_vals = intercept + slope * x_vals
+        # plt.plot(x_vals, y_vals, '--')
+        # plt.plot(x_vals, y_vals, color='grey', linestyle='--', alpha=0.3, linewidth=1.2)
+
+    def save_to_file(self):
+        """Saves the image to a file specified in the config file.
+         Prints a message if fails
+
+        Args:
+
+        Returns:
+
+        """
+        image_name = self.get_config_value('plot_output')
+        if self.figure:
+            try:
+                self.figure.write_image(image_name)
+            except FileNotFoundError:
+                print("Can't save to file " + image_name)
+            except ValueError as ex:
+                print(ex)
+        else:
+            print("Oops!  The figure was not created. Can't save.")
+
+    def write_output_file(self):
+        """
+            Writes the POFD (False alarm ratio) and PODY data points that are
+            being plotted
+
+        """
+
+        # Open file, name it based on the stat_input config setting,
+        # (the input data file) except replace the .data
+        # extension with .points1 extension
+        input_filename = self.config_obj.stat_input
+        match = re.match(r'(.*)(.data)', input_filename)
+        if match:
+            filename_only = match.group(1)
+            output_file = filename_only + ".points1"
+
+            # make sure this file doesn't already
+            # exit, delete it if it does
+            try:
+                if os.stat(output_file).st_size == 0:
+                    fileobj = open(output_file, 'a')
+                else:
+                    os.remove(output_file)
+            except FileNotFoundError as fnfe:
+                # OK if no file was found
+                pass
+
+            fileobj = open(output_file, 'a')
+            header_str = "pofd\t pody\n"
+            fileobj.write(header_str)
+            all_pody = []
+            all_pofd = []
+            for series in self.series_list:
+                pody_points = series.series_points[1]
+                pofd_points = series.series_points[0]
+                all_pody.extend(pody_points)
+                all_pofd.extend(pofd_points)
+
+            all_points = zip(all_pofd, all_pody)
+            for idx, pts in enumerate(all_points):
+                data_str = str(pts[0]) + "\t" + str(pts[1]) + "\n"
+                fileobj.write(data_str)
+
+            fileobj.close()
 
 
 def main():
@@ -153,11 +337,11 @@ def main():
             print(exc)
 
     try:
-        # create a performance diagram
-        ROCDiagram(docs)
-
-    except ValueError as value_error:
-        print(value_error)
+        r = ROCDiagram(docs)
+        r.save_to_file()
+        r.show_in_browser()
+    except ValueError as ve:
+        print(ve)
 
 
 if __name__ == "__main__":
