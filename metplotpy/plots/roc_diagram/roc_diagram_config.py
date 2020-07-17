5c723da64918a2b72e457be7fb0ba448657c3766
bikegeek <minnawin@ucar.edu>
2020-07-17 13:42:33 -0600

    Removing unused code that was commented out but not removed in previous commit.

--- metplotpy/plots/roc_diagram/roc_diagram_config.py
+++ metplotpy/plots/roc_diagram/roc_diagram_config.py
@@ -44,8 +44,7 @@ class ROCDiagramConfig(Config):
         self.add_threshold_pts = self.get_config_value('add_pt_thresh')
         self.plot_width = self.get_config_value('plot_width')
         self.plot_height = self.get_config_value('plot_height')
-        self.plot_resolution = self.get_config_value('plot_res')
-        self.plot_units = self.get_config_value('plot_units')
+        self.plot_resolution = self._get_plot_resolution()
         self.caption = self.get_config_value('plot_caption')
         self.caption_weight = self.get_config_value('caption_weight')
         self.caption_color = self.get_config_value('caption_color')
@@ -57,23 +56,28 @@ class ROCDiagramConfig(Config):
         self.marker_list = self._get_markers()
         self.linewidth_list = self._get_linewidths()
         self.linestyles_list = self._get_linestyles()
-        self.symbols_list = self._get_series_symbols()
         self.user_legends = self._get_user_legends()
+        self.add_point_thresholds = self._get_point_thresh()
+        # legend style settings as defined in METviewer
+        user_settings = self._get_legend_style()
 
-    # def _get_fcst_vars(self):
-    #     """
-    #         Override the parent's implementation.  The ROC diagram config file
-    #         currently does not require a fcst_var setting.
-    #
-    #       Args:
-    #            return an empty list if no fcst_var setting was found
-    #     """
-    #
-    #     fcst_var_val_dict = self.get_config_value('fcst_var_vals')
-    #     if not fcst_var_val_dict:
-    #         return []
-    #     else:
-    #         return [*fcst_var_val_dict.keys()]
+        # list of the x, y, and loc values for the
+        # bbox_to_anchor() setting used in determining
+        # the location of the bounding box which defines
+        # the legend.
+        self.bbox_x = float(user_settings['bbox_x'])
+        self.bbox_y = float(user_settings['bbox_y'])
+        legend_magnification = user_settings['legend_size']
+        self.legend_size = int(constants.DEFAULT_LEGEND_FONTSIZE * legend_magnification)
+        self.legend_ncol = self.get_config_value('legend_ncol')
+        legend_box = self.get_config_value('legend_box').lower()
+        if legend_box == 'n':
+            # Don't draw a box around legend labels
+            self.draw_box = False
+        else:
+            # Other choice is 'o'
+            # Enclose legend labels in a box
+            self.draw_box = True
 
     def _get_series_order(self):
         """
@@ -89,94 +93,6 @@ class ROCDiagramConfig(Config):
         series_order_list = [ord for ord in ordinals]
         return series_order_list
 
-    # def _get_plot_ci(self):
-    #     """
-    #
-    #         Args:
-    #
-    #         Returns:
-    #             list of values to indicate whether or not to plot the confidence interval for
-    #             a particular series, and which confidence interval (bootstrap or normal).
-    #
-    #     """
-    #     plot_ci_list = self.get_config_value('plot_ci')
-    #     num_series = len(self.series_ordering_zb)
-    #     ordered_ci_settings_list =[]
-    #     if not plot_ci_list:
-    #         for series in num_series:
-    #             ordered_ci_settings_list.append(" ")
-    #     ci_settings_list = [ci.upper() for ci in plot_ci_list]
-    #
-    #     # Do some checking to make sure that the values are valid (case-insensitive):
-    #     # None, boot, or norm
-    #     for ci_setting in ci_settings_list:
-    #         if ci_setting not in constants.ACCEPTABLE_CI_VALS:
-    #             raise ValueError("A plot_ci value is set to an invalid value. "
-    #                              "Accepted values are (case insensitive): "
-    #                              "None, norm, or boot. Please check your config file.")
-    #
-    #     # order the ci list according to the series_order setting (e.g. 1 2 3, 2 1 3,..., etc.)
-    #     ordered_ci_settings_list = self.create_list_by_series_ordering(ci_settings_list)
-    #
-    #     return ordered_ci_settings_list
-
-
-    # def create_list_by_series_ordering(self, setting_to_order):
-    #     """
-    #         Generate a list of series plotting settings based on what is set
-    #         in series_order in the config file.
-    #         If the series_order is specified:
-    #            series_order:
-    #             -3
-    #             -1
-    #             -2
-    #
-    #             and color is set:
-    #            color:
-    #             -red
-    #             -blue
-    #             -green
-    #
-    #            and the line widths are:
-    #            line_width:
-    #               -1
-    #               -3
-    #               -2
-    #            then the first series has a line width=3,
-    #            the second series has a line width=2,
-    #            and the third series has a line width=1
-    #
-    #         Then the following is expected:
-    #           the first series' color is 'blue'
-    #           the second series' color is 'green'
-    #           the third series' color is 'red'
-    #
-    #         This allows the user the flexibility to change marker symbols, colors, and
-    #         other line qualities between the series (lines) without having to re-order
-    #         *all* the values.
-    #
-    #         Args:
-    #
-    #             setting_to_order:  the name of the setting (eg line_width) to be
-    #                                ordered based on the order indicated
-    #                                in the config file under the series_order setting.
-    #
-    #         Returns:
-    #             a list reflecting the order that is consistent with what was set in series_order
-    #
-    #     """
-    #
-    #     # order the ci list according to the series_order setting
-    #     ordered_settings_list = []
-    #
-    #     # Make the series ordering list zero-based to sync with Python's zero-based counting
-    #     series_ordered_zb = [sorder - 1 for sorder in self.series_ordering]
-    #     for idx in range(len(setting_to_order)):
-    #         # find the current index's value in the zero-based series_ordering list
-    #         loc = series_ordered_zb.index(idx)
-    #         ordered_settings_list.append(setting_to_order[loc])
-    #
-    #     return ordered_settings_list
 
     def _get_plot_disp(self):
         """
@@ -194,3 +110,23 @@ class ROCDiagramConfig(Config):
         plot_display_bools_ordered = self.create_list_by_series_ordering(plot_display_bools)
         return plot_display_bools_ordered
 
+
+    def _get_point_thresh(self):
+        """
+            Retrieve the value (true/false) of the add_point_threshold
+            setting in the config file.  This determines whether (or not)
+            to label the corresponding threshold values.
+
+            Args:
+
+            Returns:
+                True if add_point_thresholds is set to true
+                False otherwise
+
+        """
+        if self.get_config_value('add_point_thresholds'):
+            response = self.get_config_value('add_point_thresholds').lower()
+            if response == "true":
+                return True
+            else:
+                return False
