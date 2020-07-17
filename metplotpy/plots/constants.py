a257db0f815241eaeb2140f8ecd0fdbec3b8f64f
bikegeek <minnawin@ucar.edu>
2020-07-17 14:12:07 -0600

    remove unused dictionary for linestyles

--- metplotpy/plots/constants.py
+++ metplotpy/plots/constants.py
@@ -8,6 +8,11 @@ METviewer values are keys, Matplotlib representations are the values.
 __author__ = 'Minna Win'
 __email__ = 'met_help@ucar.edu'
 
+# CONVERSION FACTORS
+
+# used to convert plot units in mm to
+# inches, so we can pass in dpi to matplotlib
+MM_TO_INCHES = 0.03937008
 
 # Available Matplotlib Line styles
 # ':'  ...
@@ -25,9 +30,13 @@ __email__ = 'met_help@ucar.edu'
 # h histogram like (no line style, this is unsupported)
 # n none (no line style)
 
-LINESTYLE_DICT = {'p': ':', 'l': '--', 'o': '-.', 'b': '-',
-                  's': '-.', 'h': ' ', 'n': ' '}
-
+# linestyles can be indicated by "long" name (points, lines, etc.) or
+# by single letter designation ('p', 'n', etc)
+LINESTYLE_BY_NAMES = {'solid': '-', 'points': ':', 'lines': '--', 'overplotted': '-.',
+                      'joined lines': '-', 'stairstep': '-.',
+                      'histogram': ' ', 'none': ' ', 'p': ':',
+                      'l': '--', 'o': '-.', 'b': '-',
+                      's': '-.', 'h': ' ', 'n': ' '}
 
 ACCEPTABLE_CI_VALS = ['NONE', 'BOOT', 'NORM']
 
@@ -38,6 +47,7 @@ DEFAULT_TITLE_FONTSIZE = 10
 # Default size used in plotly legend text
 DEFAULT_LEGEND_FONTSIZE = 12
 AVAILABLE_MARKERS_LIST = ["o", "^", "s", "d", "H", "."]
-PCH_TO_MATPLOTLIB_MARKER = {'20':'.', '19':'o', '17':'^', '1':'H', '18':'d', '15':'s'}
-
-
+PCH_TO_MATPLOTLIB_MARKER = {'20': '.', '19': 'o', '17': '^', '1': 'H',
+                            '18': 'd', '15': 's', 'small circle': '.',
+                            'circle': 'o', 'square': 's',
+                            'triangle': '^', 'rhombus': 'd', 'ring': 'h'}
