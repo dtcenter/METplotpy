b991061bf22ed6d06e8d6f2b1dc07ac44eb63bec
bikegeek <minnawin@ucar.edu>
2020-07-31 11:01:32 -0600

    Added _get_series_inner_dict() to get the inner dictionary representation of the series_val_n dictionary. This information will be useful when subsetting the pandas dataframe representation of the input data.

--- metplotpy/plots/roc_diagram/roc_diagram_config.py
+++ metplotpy/plots/roc_diagram/roc_diagram_config.py
@@ -34,6 +34,8 @@ class ROCDiagramConfig(Config):
         self.series_ordering_zb = [sorder - 1 for sorder in self.series_ordering]
 
         # settings that are unique to ROC diagrams
+        self.series_inner_dict1 = self._get_series_inner_dict(1)
+        self.series_inner_dict2 = self._get_series_inner_dict(2)
         self.stat_input = self.get_config_value('stat_input')
         # Contingency table count line type
         self.linetype_ctc = self.get_config_value('roc_ctc')
@@ -41,7 +43,6 @@ class ROCDiagramConfig(Config):
         self.linetype_pct = self.get_config_value('roc_pct')
         # Supported values for stat_curve are none, mean, and median
         self.plot_stat = self.get_config_value('stat_curve')
-        self.add_threshold_pts = self.get_config_value('add_pt_thresh')
         self.plot_width = self.calculate_plot_dimension('plot_width', 'pixels')
         self.plot_height = self.calculate_plot_dimension('plot_height', 'pixels')
         self.plot_resolution = self._get_plot_resolution()
@@ -79,6 +80,83 @@ class ROCDiagramConfig(Config):
             self.draw_box = True
 
 
+    def _get_series_inner_dict(self, index):
+        """
+            Get a dictionary containing the inner key-value pairs. This information
+            will be used to subset the PCT or CTC data (represented by a
+            pandas dataframe) for the ROC diagram.
+
+            The value of this inner dictionary corresponds to the column name of the
+            dataframe, and is saved as the key in this new dictionary.  The key of the
+            inner dictionary corresponds to the row of interest, and is saved as the
+            value in the new dictionary.
+
+            For example:
+            series_val_1:
+               model:
+                  - GFS
+                  - WRF
+               vx_mask:
+                  - FULL
+
+            The inner dictionary (from the above configuration file entry)
+            looks like this: {'model': ['GFS', 'WRF'], 'vx_mask':'FULL'}
+            and we want to subset the data where (model == GFS and vx_mask == FULL) for one
+            permutation/series and (model == WRF and vx_mask == FULL)
+            for the second permutation/series
+            (i.e. key = row value of interest and value = corresponding column header).
+
+            Our new dictionary looks like this:
+
+            {'GFS':'model', 'WRF':'model','FULL':'vx_mask'}
+
+            now we can readily determine which row value of interest corresponds to a column header.
+
+
+            We also need to support this (and other) scenario(s):
+            series_val_1:
+                model:
+                   - GFS
+                   - GALWEM
+
+            where the inner dictionary looks like: {'model': ['GFS', 'GALWEM']} and our
+            new dictionary looks like:
+            {'GFS':'model', 'GALWEM':'model'}
+
+
+            Args:
+                index:  The number defining which of series_vals_1 or series_vals_2 to consider
+
+            Returns:
+                val_inner_dict: the inner dictionary
+                                of the series_vals dictionary re-organized,
+                                where the key is the row value of interest
+                                and the value corresponds to the column header
+
+        """
+        val_inner_dict = {}
+
+        if index == 1:
+            # evaluate series_val_1 setting
+            series_val_dict = self.get_config_value('series_val_1')
+        elif index == 2:
+            # evaluate series_val_2 setting
+            series_val_dict = self.get_config_value('series_val_2')
+        else:
+            raise ValueError('Index value must be either 1 or 2')
+
+        # return empty dictionary if series_val_dict is empty
+        if not series_val_dict:
+            return {}
+
+        for k,v in series_val_dict.items():
+            # Sometimes the value consists of a list of more
+            # than one item:
+            for idx, val in enumerate(v):
+                val_inner_dict[v[idx]] = k
+
+
+        return val_inner_dict
 
     def _get_series_order(self):
         """
@@ -125,7 +203,7 @@ class ROCDiagramConfig(Config):
                 False otherwise
 
         """
-        if self.get_config_value('add_point_thresholds'):
+        if str(self.get_config_value('add_point_thresholds')):
             response = str(self.get_config_value('add_point_thresholds')).lower()
             # Treat the value as a string, as this is what we
             # will get from METviewer
@@ -133,3 +211,31 @@ class ROCDiagramConfig(Config):
                 return True
             else:
                 return False
+
+
+    def _get_fcst_vars(self, index):
+        """
+           Retrieve a list of the inner keys (fcst_vars) to the fcst_var_val dictionary.
+
+           Args:
+              index: identifier used to differentiate between fcst_var_val_1 and
+                     fcst_var_val_2 config settings
+           Returns:
+               a list containing all the fcst variables requested in the
+               fcst_var_val setting in the config file.  This will be
+               used to subset the input data that corresponds to a particular series.
+
+        """
+        all_fcst_vars = []
+        if index == 1:
+            fcst_var_val_dict = self.get_config_value('fcst_var_val_1')
+            if fcst_var_val_dict:
+                all_fcst_vars = []
+        elif index == 2:
+            fcst_var_val_dict = self.get_config_value('fcst_var_val_2')
+            if fcst_var_val_dict:
+                all_fcst_vars = []
+        else:
+            all_fcst_vars = []
+
+        return all_fcst_vars
