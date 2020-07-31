3d482d223773a36bf3707dfb2859c7c02cfe188f
bikegeek <minnawin@ucar.edu>
2020-07-31 12:04:16 -0600

    Properly subsetting input data when a permutation of series_val values exists.

--- metplotpy/plots/roc_diagram/roc_diagram_series.py
+++ metplotpy/plots/roc_diagram/roc_diagram_series.py
@@ -7,6 +7,7 @@ __email__ = 'met_help@ucar.edu'
 import pandas as pd
 import metcalcpy.util.ctc_statistics as cstats
 import metcalcpy.util.pstd_statistics as pstats
+import metcalcpy.util.utils as utils
 from plots.series import Series
 
 class ROCDiagramSeries(Series):
@@ -36,8 +37,20 @@ class ROCDiagramSeries(Series):
 
 
         """
+        # Subset data based on self.all_series_vals that we acquired from the
+        # config file
+        input_df = self.input_data
+        series_num = self.series_order
+        perm = utils.create_permutations(self.all_series_vals)
+        if len(self.all_series_vals) > 0 :
+            cur_perm = perm[series_num]
+            subset_df = self._subset_data(input_df, cur_perm)
+        else:
+            # no subsetting of data required, no series_val_1 values
+            # were specified in the config file.
+            subset_df = input_df.copy()
         if self.config.linetype_ctc:
-            df_roc = cstats.calculate_ctc_roc(self.input_data)
+            df_roc = cstats.calculate_ctc_roc(subset_df)
             pody = df_roc['pody']
             pody = pd.Series([1]).append(pody, ignore_index=True)
             pody = pody.append(pd.Series([0]), ignore_index=True)
@@ -49,10 +62,8 @@ class ROCDiagramSeries(Series):
             thresh = thresh.append(pd.Series(['']), ignore_index=True)
             return pofd, pody, thresh
 
-
         elif self.config.linetype_pct:
-            roc_df = pstats._calc_pct_roc(self.input_data)
-
+            roc_df = pstats._calc_pct_roc(subset_df)
             pody = roc_df['pody']
             pody = pd.Series([1]).append(pody, ignore_index=True)
             pody = pody.append(pd.Series([0]), ignore_index=True)
@@ -65,3 +76,40 @@ class ROCDiagramSeries(Series):
             return pofd, pody, thresh
         else:
             raise ValueError('error neither ctc or pct linetype ')
+
+    def _subset_data(self, df_full, permutation):
+        '''
+            Subset the input dataframe, iterating over the column and rows of interest
+
+            Args:
+               @param df_full:          The pandas dataframe representation of the full
+                                        input data.
+
+               @param permutation:      A list representing a permutation/series of
+                                        interest (e.g. ['model', 'vx_mask'],
+                                        which represent the series values of interest
+                                        that are specified in the config file.
+
+            Returns:
+                df_subset:  The portion of the full dataset that corresponds to the column header(s)
+                            and rows of interest.
+        '''
+
+
+        df_subset = df_full.copy()
+
+        # only supporting series_val_1 for ROC diagrams, so we are
+        # only interested in the series_inner_dict1
+        inner_dict = self.config.series_inner_dict1
+
+        for perm in permutation:
+            for k,v in self.config.series_inner_dict1.items():
+                if perm == k:
+                    column_header = v
+                    row_of_interest = perm
+
+            df_subset = df_subset[df_subset[column_header] == row_of_interest]
+
+
+
+        return df_subset
