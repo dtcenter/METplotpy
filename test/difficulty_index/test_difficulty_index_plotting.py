0330b4534f62031272c3dc229b167a55d1a8fb5d
Lindsay Blank <lblank@kiowa.rap.ucar.edu>
2020-10-23 15:35:31 -0600

    Added warning filter

--- test/difficulty_index/test_difficulty_index_plotting.py
+++ test/difficulty_index/test_difficulty_index_plotting.py
@@ -1,6 +1,7 @@
 import example_difficulty_index as edi  
 from metcalcpy.compare_images import CompareImages
 import pytest
+import warnings
 
 def test_difficulty_index_plot():
     """
@@ -8,6 +9,9 @@ def test_difficulty_index_plot():
     one generated.  Invoke the plot_difficulty_index.py
     script to generate the difficulty_index.png
     """
+    
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+
     file1 = 'swh_North_Pacific_5dy_ensemble.npz'
     edi.main()
     comparison = CompareImages('diff_index_expected.png', 'swh_North_Pacific_difficulty_index_9_00_feet.png')
