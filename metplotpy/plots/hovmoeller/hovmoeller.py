4211f7216de35543bcffccdc5bd19522a3dae241
davidfillmore <fillmore.winslow.david@gmail.com>
2020-11-16 11:34:09 -0700

    Added test command line arguments.

--- metplotpy/plots/hovmoeller/hovmoeller.py
+++ metplotpy/plots/hovmoeller/hovmoeller.py
@@ -37,7 +37,38 @@ class Hovmoeller(MetPlot):
 
         super().__init__(parameters, default_conf_filename)
 
+
 if __name__ == "__main__":
+    """
+    Parse command line arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--datadir', type=str,
+                        default=os.getenv('DATA_DIR'),
+                        help='top-level data directory (default $DATA_DIR)')
+    parser.add_argument('--input', type=str,
+                        required=True,
+                        help='input file name')
+    parser.add_argument('--logfile', type=str,
+                        default=sys.stdout,
+                        help='log file (default stdout)')
+    parser.add_argument('--debug', action='store_true',
+                        help='set logging level to debug')
+    args = parser.parse_args()
+
+    """
+    Setup logging
+    """
+    logging_level = logging.DEBUG if args.debug else logging.INFO
+    logging.basicConfig(stream=args.logfile, level=logging_level)
+
+    if os.path.isdir(args.datadir):
+        logging.info(args.datadir)
+    else:
+        logging.error(args.datadir + ' not found')
+        sys.exit(1)
+    logging.info(args.input)
+
 
     data = None
     plot = Hovmoeller(None, data)
