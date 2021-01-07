e85d6df616eb470e3aa33dd7c6d3f740243180a2
Lindsay Blank <lblank@kiowa.rap.ucar.edu>
2021-01-07 16:29:45 -0700

    Updated package paths to reflect new directory structure

--- metplotpy/plots/difficulty_index/plot_difficulty_index.py
+++ metplotpy/plots/difficulty_index/plot_difficulty_index.py
@@ -12,7 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from metcalcpy.calc_difficulty_index import forecast_difficulty as di
 from metcalcpy.piecewise_linear import PiecewiseLinear as plin
-import metplotpy.difficulty_index.mycolormaps as mcmap
+import metplotpy.plots.difficulty_index.mycolormaps as mcmap
 
 __author__ = 'Bill Campbell (NRL) and Lindsay Blank (NCAR)'
 __version__ = '0.1.0'
