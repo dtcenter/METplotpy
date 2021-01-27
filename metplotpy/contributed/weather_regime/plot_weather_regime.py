d57183522937429504153f1c71864388e9341b9b
Christina Kalb <kalb@cheyenne2.ib0.cheyenne.ucar.edu>
2021-01-27 15:27:04 -0700

    Updates to EOF plotting

--- metplotpy/contributed/weather_regime/plot_weather_regime.py
+++ metplotpy/contributed/weather_regime/plot_weather_regime.py
@@ -6,6 +6,7 @@ from pylab import *
 import cmocean
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 from cartopy.util import add_cyclic_point
 import numpy as np
 import sys
@@ -13,9 +14,6 @@ sys.path.insert(0, "/glade/u/home/kalb/UIUC/METcalcpy/")
 import metcalcpy.util.utils as util
 
 def plot_elbow(K,d,mi,line,curve,plot_title,output_plotname):
-
-    #lw = 2
-    #rc('axes',linewidth=1.5)
 
     plt.plot(K[int(mi)],d[mi]*-1,'*k')
     plt.plot(K, np.array(curve), 'kx-')
@@ -30,15 +28,45 @@ def plot_elbow(K,d,mi,line,curve,plot_title,output_plotname):
     plt.savefig(full_output_plot,format=fmt,dpi=400, bbox_inches='tight')
 
 
-def plot_K_means(inputi,wrnum,lon,lat,perc,output_plotname):
+def plot_eof(eof,wrnum,variance_fractions,lons,lats,output_plotname,plevels):
+
+    middleIndex = int((len(lons) - 1)/2)
+    lon0 = lons[middleIndex]
+
+    remp = wrnum%2
+    if remp==0:
+        nrows = wrnum/2
+    else:
+        nrows = (wrnum+1)/2
+
+    #Plot leading 6 modes
+    tran = ccrs.PlateCarree(central_longitude=lon0)
+    proj = tran
+    fig = plt.figure(figsize=(10,10))
+    for i in np.arange(0,wrnum,1):
+        corr = eof[i]
+        ax1 = fig.add_subplot(nrows,2,i+1,projection=proj)
+        contourf(lons,lats,corr,plevels,transform=ccrs.PlateCarree(),cmap = cmocean.cm.balance,extend='both')
+        if (wrnum - i) <= 2:
+            plt.colorbar(orientation ='horizontal',pad=0.01)
+        ax1.add_feature(cfeature.BORDERS)
+        ax1.add_feature(cfeature.STATES)
+        ax1.add_feature(cfeature.COASTLINE)
+        plt.title('EOF '+str(i+1)+' ('+str(round(variance_fractions[i],2))+' %)')
+
+    plt.tight_layout()
+    fmt='png'
+    full_output_plot = output_plotname + "." + fmt
+    plt.show()
+
+
+def plot_K_means(inputi,wrnum,lon,lat,perc,output_plotname,plevels):
 
     lon,lonsort = util.convert_lons_indices(lon,-180,360)
 
     ### Plot the clusters
     middleIndex = int((len(lon) - 1)/2)
     lon0 = lon[middleIndex]
-
-    r = np.arange(-80,81,10)#*2.0
 
     ii=np.arange(0,wrnum,1)
     remp = wrnum%2
@@ -54,7 +82,10 @@ def plot_K_means(inputi,wrnum,lon,lat,perc,output_plotname):
     for g1 in np.arange(0,wrnum,1):
         g = ii[g1]
         ax1 = fig.add_subplot(nrows,2,g1+1,projection=proj)
-        contourf(lon,lat,inputi[g],r,transform=ccrs.PlateCarree(),cmap = cmocean.cm.balance,extend="both")
+        if plevels:
+            contourf(lon,lat,inputi[g],plevels,transform=ccrs.PlateCarree(),cmap = cmocean.cm.balance,extend="both")
+        else:
+            contourf(lon,lat,inputi[g],transform=ccrs.PlateCarree(),cmap = cmocean.cm.balance,extend="both")
         ax1.coastlines(resolution='50m', color='gray', linewidth=1.25)
         if (wrnum - g1) <= 2:
             plt.colorbar(orientation='horizontal', fraction=0.086, pad=0.05)
@@ -65,5 +96,3 @@ def plot_K_means(inputi,wrnum,lon,lat,perc,output_plotname):
     fmt='png'
     full_output_plot = output_plotname + "." + fmt
     plt.savefig(full_output_plot,format=fmt,dpi=400, bbox_inches='tight')
-
-    plt.show()
