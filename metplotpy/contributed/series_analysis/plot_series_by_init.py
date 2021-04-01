b0e75e45b757f8bf914dea7f2b7b6f072f806a00
bikegeek <minnawin@ucar.edu>
2021-04-01 17:44:36 -0600

    Refactored to suppress MatplotlibFutureDeprecationWarning and to use simple yaml config file for settings.

--- metplotpy/contributed/series_analysis/plot_series_by_init.py
+++ metplotpy/contributed/series_analysis/plot_series_by_init.py
@@ -3,37 +3,64 @@ Generates static plots (png) from netcdf output from performing series analysis 
 """
 
 import os
+import errno
+import warnings
 import matplotlib.pyplot as plt
+import matplotlib.cbook
 from matplotlib import cm
 import cartopy.crs as ccrs
 from cartopy.util import add_cyclic_point
 #pylint: disable=import-error
 from netCDF4 import Dataset
+import plot_util as util
+
+# ignore the MatplotlibFutureDeprecation warning which does not affect this code
+# since it was developed with matplotlib 3.3
+warnings.simplefilter(action='ignore', category=matplotlib.cbook.mplDeprecation)
 
 
-def create_plot(input_nc_file_dir, input_nc_filename, variable_name, level,
-                storm_number, output_filename_base, output_dir, background_on=False):
+
+def create_plot():
     '''
         Generates the plots (png) for each var-level-stat combination created
         by the series analysis by init.
         Reads the input netcdf file and gathers the lat, lon, FBAR and OBAR
         values into numpy arrays
-    :param input_nc_file_dir The directory where the netcdf input file resides
-    :param input_nc_file:  The name of the netcdf input file containing the variable of interest
-    :param variable_name:   The name of the variable of interest, e.g. TEMP.
-    :pararm level:          The level corresponding to the variable of interest,
-                            e.g. Z2, L1, P500, etc.
-    :param storm_number    The storm number, used in the title of the plot.
-    :param output_filename_base The "base" output filename to be used for FBAR and OBAR plots.
-    :param output_dir      The directory where you wish to store the output plots.
-    :param background_on:  Background map with coastlines are by default turned "off".
+
 
     :return:
 
+    :raises errno.EEXIST, FileNotFoundError
+
     '''
 
+    # Retrieve the settings indicating the storm, variables, levels, etc of interest from the yaml config file,
+    # series_init.yaml
+    config = util.read_yaml_config()
+    input_nc_filename = config['input_nc_file']
+    input_nc_file_dir = config['input_nc_file_dir']
+    variable_name = config['variable_name']
+    level = config['level']
+    storm_number = config['storm_number']
+    output_filename_base = config['output_filename_base']
+    output_dir = config['output_dir']
+    background_on_setting = config['background_on']
+
+    if background_on_setting.upper() == 'TRUE':
+        background_on = True
+    else:
+        background_on = False
+
+    # create the output directory if it doesn't exist
     try:
-        input_nc_file = os.path.join(input_nc_file_dir, input_nc_filename)
+        os.makedirs(output_dir)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+    try:
+        input_nc_file_storm_dir = os.path.join(input_nc_file_dir, storm_number)
+        input_nc_file = os.path.join(input_nc_file_storm_dir, input_nc_filename)
         file_handle = Dataset(input_nc_file, mode='r')
     except FileNotFoundError:
         print("File ", input_nc_file, " does not exist.")
@@ -127,35 +154,14 @@ def create_plot(input_nc_file_dir, input_nc_filename, variable_name, level,
 
 def main():
     """
-    Place where the user sets the input necessary to "customize" static plots to
-    reflect the storm number of interest, variable, level, and other things affecting
-    the plot.
+       Invoke create_plot() to generate png OBAR and FBAR plots for specified storm, level, variable of the
+       METplus series analysis output.
 
     :return:
     """
-    # The storm number of interest
-    storm_number = 'ML1200972014'
-    # The input directory where the netcdf files created by the series analysis
-    # (by initialization times) are saved
 
-    nc_input_dir_base = '/d1/projects/METplus/METplus_Plotting_Data/series_by_init/20141214_00/'
-    nc_input_dir = nc_input_dir_base + storm_number
-    output_dir = nc_input_dir
-
-    # The name of the netcdf file of interest
-    nc_input_filename = 'series_TMP_Z2.nc'
-    variable_name = 'TMP'
-    level = 'Z2'
-
-    # The output file name base which will look something like ML1200972014/TMP_Z2, and
-    # the .png extension will be added to the filename when the plot is created.
-    obar_fbar_output = 'TMP_Z2'
-
-    # By default, this is set to False, set to True if you want the coastlines plotted.
-    include_background = True
     # Invoke the function that generates the plot
-    create_plot(nc_input_dir, nc_input_filename, variable_name, level,
-                storm_number, obar_fbar_output, output_dir, include_background)
+    create_plot()
 
 
 if __name__ == "__main__":
