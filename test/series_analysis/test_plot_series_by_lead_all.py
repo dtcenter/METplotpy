f2177f298716a4bcd058b62921c506df71de724f
bikegeek <minnawin@ucar.edu>
2021-04-02 11:22:19 -0600

    Added tests for series analysis by lead for groupings.

--- test/series_analysis/test_plot_series_by_lead_all.py
+++ test/series_analysis/test_plot_series_by_lead_all.py
@@ -5,10 +5,11 @@
 
 """
 
-import sys
+import os, sys, re
 import yaml
 sys.path.append("../../")
 from metplotpy.contributed.series_analysis.plot_series_by_lead_all import PlotSeriesByLeadAll
+from metplotpy.contributed.series_analysis.plot_series_by_grouping import PlotSeriesByGrouping
 
 def test_get_info():
     """
@@ -45,7 +46,7 @@ def test_get_info():
 
     # Verify that we have found the number of netcdf files that were
     # expected (from running the feature relative use case corresponding
-    # to using the series_by_lead_all_fhrs.conf).
+    # to using the series_by_lead_all_fhrs.yaml).
     num_expected = len(expected_files)
     match_counter = 0
     for cur_file in series_by_lead_tuples:
@@ -55,4 +56,109 @@ def test_get_info():
     assert (match_counter == num_expected)
 
 
+def test_series_lead_group_plots():
+    ''' Test that the expected png files are created by the plot_series_by_grouping module'''
 
+    config_file = "./series_lead_group.yaml"
+
+    with open(config_file, 'r') as stream:
+        try:
+            config = yaml.load(stream, Loader=yaml.FullLoader)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    psg = PlotSeriesByGrouping(config)
+    input_nc_file_dir = psg.config['input_nc_file_dir']
+    output_dir = psg.config['output_dir']
+    background_on_setting = psg.config['background_on']
+    if background_on_setting.upper()  == 'TRUE':
+        background_on = True
+    else:
+        background_on = False
+
+    filename_regex = psg.config['png_plot_filename_regex']
+    expected_files = [
+        '/Volumes/d1/minnawin/METplus_Plotting_Output/series_analysis_lead_group/plot/'
+        'series_F000_to_F018_TMP_Z2_fbar.png',
+        '/Volumes/d1/minnawin/METplus_Plotting_Output/series_analysis_lead_group/plot/'
+        'series_F000_to_F018_TMP_Z2_obar.png',
+        '/Volumes/d1/minnawin/METplus_Plotting_Output/series_analysis_lead_group/plot/'
+        'series_F024_to_F042_TMP_Z2_fbar.png',
+        '/Volumes/d1/minnawin/METplus_Plotting_Output/series_analysis_lead_group/plot/'
+        'series_F024_to_F042_TMP_Z2_obar.png'
+    ]
+
+    psg.create_plots(input_nc_file_dir, output_dir, background_on, filename_regex)
+
+    # Verify that we have found the number of png files that were
+    # expected (from running the feature relative use case corresponding
+    # to using the series_lead_group.yaml).
+    output_files_list = []
+    match_counter = 0
+    for root, dirs, files in os.walk(output_dir):
+        for file in files:
+            # match = re.search('series_F([0-9]{3})_(HGT|TMP)_(P|Z)([0-9]{3}).nc', file)
+            match = re.search('series_F([0-9]{1,3})_to_F([0-9]{1,3})_(HGT|TMP)_(P|Z)([0-9]{1,3})(.*).png', file)
+            if match:
+                full_file = os.path.join(output_dir, file)
+                output_files_list.append(full_file)
+                match_counter += 1
+
+    num_expected = len(expected_files)
+    for cur_file in output_files_list:
+        if cur_file in expected_files:
+            match_counter = match_counter + 1
+
+    # print("Number of matches: ", match_counter)
+    assert (match_counter == num_expected)
+
+
+def test_series_lead_group_animations():
+    ''' Test that the expected gif files (animations) are created by the plot_series_by_grouping module'''
+
+    config_file = "./series_lead_group.yaml"
+
+    with open(config_file, 'r') as stream:
+        try:
+            config = yaml.load(stream, Loader=yaml.FullLoader)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    psg = PlotSeriesByGrouping(config)
+    input_nc_file_dir = psg.config['input_nc_file_dir']
+    output_dir = psg.config['output_dir']
+    background_on_setting = psg.config['background_on']
+    if background_on_setting.upper()  == 'TRUE':
+        background_on = True
+    else:
+        background_on = False
+
+    filename_regex = psg.config['png_plot_filename_regex']
+    expected_files = [
+        '/Volumes/d1/minnawin/METplus_Plotting_Output/series_analysis_lead_group/plot/series_TMP_Z2_fbar.gif',
+        '/Volumes/d1/minnawin/METplus_Plotting_Output/series_analysis_lead_group/plot/series_TMP_Z2_obar.gif'
+    ]
+
+    psg.create_plots(input_nc_file_dir, output_dir, background_on, filename_regex)
+
+    # Verify that we have found the number of png files that were
+    # expected (from running the feature relative use case corresponding
+    # to using the series_lead_group.yaml).
+    output_files_list = []
+    match_counter = 0
+    for root, dirs, files in os.walk(output_dir):
+        for file in files:
+            # match = re.search('series_F([0-9]{3})_(HGT|TMP)_(P|Z)([0-9]{3}).nc', file)
+            match = re.search('series_(HGT|TMP)_(P|Z)([0-9]{1,3})(.*).gif', file)
+            if match:
+                full_file = os.path.join(output_dir, file)
+                output_files_list.append(full_file)
+                match_counter += 1
+
+    num_expected = len(expected_files)
+    for cur_file in output_files_list:
+        if cur_file in expected_files:
+            match_counter = match_counter + 1
+
+    print("Number of matches: ", match_counter)
+    # assert (match_counter == num_expected)
