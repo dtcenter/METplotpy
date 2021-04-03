4ebd702793e291d43c65cbfa97e196367e13b2ff
bikegeek <minnawin@ucar.edu>
2021-04-03 15:56:54 -0600

    Add test for animate_series_by_lead_all

--- test/series_analysis/test_plot_series_by_lead_all.py
+++ test/series_analysis/test_plot_series_by_lead_all.py
@@ -10,6 +10,8 @@ import yaml
 sys.path.append("../../")
 from metplotpy.contributed.series_analysis.plot_series_by_lead_all import PlotSeriesByLeadAll
 from metplotpy.contributed.series_analysis.plot_series_by_grouping import PlotSeriesByGrouping
+from metplotpy.contributed.series_analysis.animate_series_by_lead_all import AnimateSeriesByLeadAll
+from metplotpy.contributed.series_analysis import animate_utilities as au
 
 def test_get_info():
     """
@@ -162,3 +164,68 @@ def test_series_lead_group_animations():
 
     print("Number of matches: ", match_counter)
     # assert (match_counter == num_expected)
+
+
+def test_animation_series_by_lead():
+    ''' Test that the expected gif files (animations) are created by the
+        animate_series_by_lead module.
+    '''
+
+    config_file = "./animate.yaml"
+
+    with open(config_file, 'r') as stream:
+        try:
+            config = yaml.load(stream, Loader=yaml.FullLoader)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    asbl = AnimateSeriesByLeadAll(config)
+    # Get the values that were set in the yaml config file animate.yaml
+    input_dir = asbl.config['input_dir']
+    output_dir = asbl.config['output_dir']
+    fhrs_list = asbl.config['fhrs_list']
+    variable = asbl.config["variable"]
+    level_type = asbl.config['level_type']
+    level = asbl.config['level']
+    filename_regex = asbl.config['filename_regex']
+    animation_duration_secs = asbl.config['animation_duration_secs']
+    # list of statistics of interest
+    statistics_of_interest = asbl.config['statistic_of_interest']
+
+    # Animate the plots corresponding to the statistics of interest for the corresponding forecast, variable, and level
+    for statistic in statistics_of_interest:
+        stat_files = asbl.collect_files_to_animate(input_dir, fhrs_list, variable,
+                                                   level_type, level, statistic)
+        # create output filename for statistic animation (gif) file
+        output_filename = asbl.create_output_filename(output_dir, stat_files[0], filename_regex)
+        au.create_gif(animation_duration_secs, stat_files, output_filename)
+
+    expected_files = [
+        '/Volumes/d1/minnawin/METplus_Plotting_Output/series_by_lead_all_fhrs/output/series_animate_python'
+        '/series_TMP_Z2_fbar.gif',
+        '/Volumes/d1/minnawin/METplus_Plotting_Output/series_by_lead_all_fhrs/output/series_animate_python'
+        '/series_TMP_Z2_obar.gif'
+    ]
+
+    # Verify that we have found the number of png files that were
+    # expected (from running the feature relative use case corresponding
+    # to using the series_lead_group.yaml).
+    output_files_list = []
+    match_counter = 0
+    for root, dirs, files in os.walk(output_dir):
+        for file in files:
+            # match = re.search('series_F([0-9]{3})_(HGT|TMP)_(P|Z)([0-9]{3}).nc', file)
+            match = re.search('series_(HGT|TMP)_(P|Z)([0-9]{1,3})(.*).gif', file)
+            if match:
+                full_file = os.path.join(output_dir, file)
+                output_files_list.append(full_file)
+                match_counter += 1
+
+    num_expected = len(expected_files)
+    for cur_file in output_files_list:
+        if cur_file in expected_files:
+            match_counter = match_counter + 1
+
+    print("Number of matches: ", match_counter)
+    # assert (match_counter == num_expected)
+
