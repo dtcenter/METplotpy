47eb7c228625aaaad3a5f389e59e7d834c55a4e8
bikegeek <minnawin@ucar.edu>
2021-04-03 16:16:55 -0600

    Removed unneccessary code that confounds final results

--- test/series_analysis/test_plot_series_by_lead_all.py
+++ test/series_analysis/test_plot_series_by_lead_all.py
@@ -7,11 +7,13 @@
 
 import os, sys, re
 import yaml
+import pytest
 sys.path.append("../../")
 from metplotpy.contributed.series_analysis.plot_series_by_lead_all import PlotSeriesByLeadAll
 from metplotpy.contributed.series_analysis.plot_series_by_grouping import PlotSeriesByGrouping
 from metplotpy.contributed.series_analysis.animate_series_by_lead_all import AnimateSeriesByLeadAll
 from metplotpy.contributed.series_analysis import animate_utilities as au
+
 
 def test_get_info():
     """
@@ -57,7 +59,6 @@ def test_get_info():
 
     assert (match_counter == num_expected)
 
-
 def test_series_lead_group_plots():
     ''' Test that the expected png files are created by the plot_series_by_grouping module'''
 
@@ -95,7 +96,6 @@ def test_series_lead_group_plots():
     # Verify that we have found the number of png files that were
     # expected (from running the feature relative use case corresponding
     # to using the series_lead_group.yaml).
-    output_files_list = []
     match_counter = 0
     for root, dirs, files in os.walk(output_dir):
         for file in files:
@@ -103,17 +103,12 @@ def test_series_lead_group_plots():
             match = re.search('series_F([0-9]{1,3})_to_F([0-9]{1,3})_(HGT|TMP)_(P|Z)([0-9]{1,3})(.*).png', file)
             if match:
                 full_file = os.path.join(output_dir, file)
-                output_files_list.append(full_file)
                 match_counter += 1
 
     num_expected = len(expected_files)
-    for cur_file in output_files_list:
-        if cur_file in expected_files:
-            match_counter = match_counter + 1
 
     # print("Number of matches: ", match_counter)
     assert (match_counter == num_expected)
-
 
 def test_series_lead_group_animations():
     ''' Test that the expected gif files (animations) are created by the plot_series_by_grouping module'''
@@ -146,7 +141,6 @@ def test_series_lead_group_animations():
     # Verify that we have found the number of png files that were
     # expected (from running the feature relative use case corresponding
     # to using the series_lead_group.yaml).
-    output_files_list = []
     match_counter = 0
     for root, dirs, files in os.walk(output_dir):
         for file in files:
@@ -154,17 +148,10 @@ def test_series_lead_group_animations():
             match = re.search('series_(HGT|TMP)_(P|Z)([0-9]{1,3})(.*).gif', file)
             if match:
                 full_file = os.path.join(output_dir, file)
-                output_files_list.append(full_file)
                 match_counter += 1
 
     num_expected = len(expected_files)
-    for cur_file in output_files_list:
-        if cur_file in expected_files:
-            match_counter = match_counter + 1
-
-    print("Number of matches: ", match_counter)
-    # assert (match_counter == num_expected)
-
+    assert (match_counter == num_expected)
 
 def test_animation_series_by_lead():
     ''' Test that the expected gif files (animations) are created by the
@@ -210,7 +197,6 @@ def test_animation_series_by_lead():
     # Verify that we have found the number of png files that were
     # expected (from running the feature relative use case corresponding
     # to using the series_lead_group.yaml).
-    output_files_list = []
     match_counter = 0
     for root, dirs, files in os.walk(output_dir):
         for file in files:
@@ -218,14 +204,10 @@ def test_animation_series_by_lead():
             match = re.search('series_(HGT|TMP)_(P|Z)([0-9]{1,3})(.*).gif', file)
             if match:
                 full_file = os.path.join(output_dir, file)
-                output_files_list.append(full_file)
                 match_counter += 1
 
     num_expected = len(expected_files)
-    for cur_file in output_files_list:
-        if cur_file in expected_files:
-            match_counter = match_counter + 1
 
-    print("Number of matches: ", match_counter)
-    # assert (match_counter == num_expected)
+    # print("Number of matches: ", match_counter)
+    assert (match_counter == num_expected)
 
