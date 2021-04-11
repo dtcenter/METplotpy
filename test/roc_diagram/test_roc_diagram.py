0baf20ad851f5627d45c8e861a11cc921b6834f2
bikegeek <minnawin@ucar.edu>
2021-04-11 10:43:14 -0600

    Add test to verify that reversing connection order results in expected behavior

--- test/roc_diagram/test_roc_diagram.py
+++ test/roc_diagram/test_roc_diagram.py
@@ -33,6 +33,22 @@ def setup():
     # the custom_performance_diagram.yaml custom config file.
     roc.main(custom_config_filename)
 
+@pytest.fixture
+def setup_rev_points():
+    # Cleanup the plotfile and point1 output file from any previous run
+    # cleanup()
+    # Set up the METPLOTPY_BASE so that met_plot.py will correctly find
+    # the config directory containing all the default config files.
+    os.environ['METPLOTPY_BASE'] = "../../metplotpy"
+    custom_config_filename = "./CTC_ROC_thresh_reverse_pts.yaml"
+    print("\n current directory: ", os.getcwd())
+    print("\ncustom config file: ", custom_config_filename, '\n')
+
+
+    # Invoke the command to generate a Performance Diagram based on
+    # the custom_performance_diagram.yaml custom config file.
+    roc.main(custom_config_filename)
+
 def cleanup():
     # remove the performance_diagram_expected.png and plot_20200317_151252.points1 files
     # from any previous runs
@@ -91,6 +107,44 @@ def test_expected_CTC_thresh_points(setup):
     assert True
 
 
+
+def test_expected_CTC_thresh_points_reversed(setup_rev_points):
+    '''
+        For test data, verify that the points in the .points1 file
+        match what is expected (within round-off tolerance/acceptable precision) when
+        we set reverse_connection_order: 'True'.
+    :return:
+    '''
+    expected_pody = pd.Series([1,0.8457663, 0.7634846, 0.5093934, 0.1228585, 0 ])
+    expected_pofd = pd.Series([1, 0.0688293, 0.0491275, 0.0247044, 0.0048342, 0 ])
+
+    df = pd.read_csv("./CTC_ROC_thresh.points1", sep='\t', header='infer')
+    pofd = df.iloc[:, 0]
+    pody = df.iloc[:, 1]
+
+    for index, expected in enumerate(expected_pody):
+        if ctc.round_half_up(expected) - ctc.round_half_up(pody[index]) == 0.0:
+            pass
+        else:
+            assert False
+
+        # if we get here, then all elements matched in value and position
+    assert True
+
+    # do the same test for pofd
+    for index, expected in enumerate(expected_pofd):
+        if ctc.round_half_up(expected) - ctc.round_half_up(pofd[index]) == 0.0:
+            pass
+        else:
+            assert False
+
+        # if we get here, then all elements matched in value and position
+    assert True
+
+
+
+
+
 def test_images_match(setup):
     '''
         Compare an expected plot with the
@@ -106,3 +160,4 @@ def test_images_match(setup):
     actual_file = os.path.join(path, plot_file)
     comparison = CompareImages('./CTC_ROC_thresh.png',actual_file)
     assert comparison.mssim == 1
+
