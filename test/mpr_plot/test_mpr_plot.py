cfaa4f02092b6119f535bea932342c11ed37a363
TatianaBurek <tatiana@ucar.edu>
2021-05-26 11:32:14 -0600

    original version #9

---
+++ test/mpr_plot/test_mpr_plot.py
@@ -0,0 +1,60 @@
+# !!!IMPORTANT!!!
+# activate conda environment in the testing subshell
+# Replace blenny_363 with your METplus Python 3.6.3
+# conda environment name
+# !!!!!!!!
+
+# !/usr/bin/env conda run -n blenny_363 python
+
+import pytest
+import os
+from metplotpy.plots.mpr_plot import mpr_plot
+from metcalcpy.compare_images import CompareImages
+
+
+@pytest.fixture
+def setup():
+    # Cleanup the plotfile and point1 output file from any previous run
+    cleanup()
+    # Set up the METPLOTPY_BASE so that met_plot.py will correctly find
+    # the config directory containing all the default config files.
+    os.environ['METPLOTPY_BASE'] = "../../metplotpy"
+    custom_config_filename = "mpr_plot_custom.yaml"
+
+    # Invoke the command to generate a Performance Diagram based on
+    # the custom_performance_diagram.yaml custom config file.
+    mpr_plot.main(custom_config_filename)
+
+
+def cleanup():
+    # remove the line.png and .points files
+    # from any previous runs
+    try:
+        path = os.getcwd()
+        plot_file = 'mpr_plots.png'
+        os.remove(os.path.join(path, plot_file))
+    except OSError as e:
+        # Typically when files have already been removed or
+        # don't exist.  Ignore.
+        pass
+
+
+@pytest.mark.parametrize("test_input, expected",
+                         (["./mpr_plots.png", True],["./mpr_plots_expected.png", True]))
+def test_files_exist(setup, test_input, expected):
+    """
+        Checking that the plot and data files are getting created
+    """
+    assert os.path.isfile(test_input) == expected
+    cleanup()
+
+
+def test_images_match(setup):
+    """
+        Compare an expected plot with the
+        newly created plot to verify that the plot hasn't
+        changed in appearance.
+    """
+    comparison = CompareImages('./mpr_plots_expected.png', './mpr_plots.png')
+    assert comparison.mssim == 1
+    cleanup()
