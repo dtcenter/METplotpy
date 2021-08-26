a37eb931f1dd5d097c0b39bc83a8bc656755f7e2
bikegeek <minnawin@ucar.edu>
2021-08-26 12:05:36 -0600

    Github Issue #37 Added by_pressure_lvl option to indicate whether to plot range vs pressure in mb or range vs height (if not set).  Pressure in height (meters) assumes that the user is using the output from the METcalcpy vertical interpolation module.

--- metplotpy/contributed/tc_rmw/test_plot_cross_section.sh
+++ metplotpy/contributed/tc_rmw/test_plot_cross_section.sh
@@ -1,10 +1,21 @@
-export datadir=$HOME/Data/TCRMW
-export plotdir=$HOME/Plots
+#export datadir=$HOME/Data/TCRMW
+export datadir=/Volumes/d1/minnawin/METcalcpy_Data/Vert_Interp
+#export plotdir=$HOME/Plots
+export plotdir=/Volumes/d1/minnawin/METplotpy_37_vert_interp/Plots
 # export datadir=$DATA_DIR/TCRMW
 # export plotdir=$PLOT_DIR
 export filename=tc_rmw_dev_test_out.nc
+# leave empty if plotting by height (vertical interpolation used)
+# or set to True if plotting by pressure level
+export by_pressure_lvl=yes
 
 python plot_cross_section.py \
     --datadir=$datadir \
     --plotdir=$plotdir \
-    --filename=$filename
+    --filename=$filename \
+    --by_pressure_lvl=$by_pressure_lvl
+
+#python plot_cross_section.py \
+#    --datadir=$datadir \
+#    --plotdir=$plotdir \
+#    --filename=$filename \
