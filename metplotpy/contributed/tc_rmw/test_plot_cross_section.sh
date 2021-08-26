ed1513a0fa1a292f596ecea358be6fadb5f96d5b
bikegeek <minnawin@ucar.edu>
2021-08-26 15:22:07 -0600

    More instructive comment on how to use by_pressure_lvl

--- metplotpy/contributed/tc_rmw/test_plot_cross_section.sh
+++ metplotpy/contributed/tc_rmw/test_plot_cross_section.sh
@@ -5,8 +5,9 @@ export plotdir=/Volumes/d1/minnawin/METplotpy_37_vert_interp/Plots
 # export datadir=$DATA_DIR/TCRMW
 # export plotdir=$PLOT_DIR
 export filename=tc_rmw_dev_test_out.nc
-# leave empty if plotting by height (vertical interpolation used)
-# or set to True if plotting by pressure level
+# leave empty if plotting by height in meters (i.e vertical interpolation used)
+# or set to something (like 'yes' or 'True', etc) if pressure input data
+# is in pressure (mb)
 export by_pressure_lvl=yes
 
 python plot_cross_section.py \
