c5252c155be83e7bec898983634307f54d5a9297
ahijevyc <ahijevyc@ucar.edu>
2021-09-30 10:59:47 -0600

    dictionary of tendencies for each state variable

---
+++ metplotpy/contributed/fv3_physics_tend/fv3.py
@@ -0,0 +1,6 @@
+tendencies = dict(
+tmp = ['dt3dt_cnvgwd','dt3dt_deepcnv','dt3dt_lw','dt3dt_mp','dt3dt_orogwd','dt3dt_pbl','dt3dt_rdamp','dt3dt_shalcnv','dt3dt_sw','dt3dt_nophys'],
+spfh= ['dq3dt_deepcnv','dq3dt_mp','dq3dt_pbl','dq3dt_shalcnv','dq3dt_nophys'],
+ugrd= ['du3dt_cnvgwd','du3dt_deepcnv','du3dt_orogwd','du3dt_pbl','du3dt_rdamp','du3dt_shalcnv','du3dt_nophys'],
+vgrd= ['dv3dt_cnvgwd','dv3dt_deepcnv','dv3dt_orogwd','dv3dt_pbl','dv3dt_rdamp','dv3dt_shalcnv','dv3dt_nophys']
+)
