8a26a2d32a46de7cb1e0a9236473089b8ee910f5
bikegeek <minnawin@ucar.edu>
2021-11-03 19:58:12 -0600

    Github #37 Remove the --by-pressure-level argument and instead utilize the value of the vertical_coord_type_in in the plot_cross_section.yaml config file.

--- metplotpy/contributed/tc_rmw/test_plot_cross_section.sh
+++ metplotpy/contributed/tc_rmw/test_plot_cross_section.sh
@@ -1,20 +1,17 @@
-#export datadir=$HOME/Data/TCRMW
+
 export datadir=/Volumes/d1/minnawin/METcalcpy_Data/Vert_Interp
-#export plotdir=$HOME/Plots
+
 export plotdir=/Volumes/d1/minnawin/METplotpy_37_vert_interp/Plots
-# export datadir=$DATA_DIR/TCRMW
-# export plotdir=$PLOT_DIR
+
 export filename=tc_rmw_dev_test_out.nc
-# leave empty if plotting by height in meters (i.e vertical interpolation used)
-# or set to something (like 'yes' or 'True', etc) if pressure input data
-# is in pressure (mb)
-export by_pressure_lvl=yes
+
+export configfile=/Volumes/d1/minnawin/METplotpy_37_vert_interp/METplotpy/metplotpy/contributed/tc_rmw/plot_cross_section.yaml
 
 python plot_cross_section.py \
     --datadir=$datadir \
     --plotdir=$plotdir \
     --filename=$filename \
-    --by_pressure_lvl=$by_pressure_lvl
+    --config=$configfile
 
 #python plot_cross_section.py \
 #    --datadir=$datadir \
