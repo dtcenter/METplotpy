024c075d1b9d240b75d0bb014b169a7fc725e329
bikegeek <minnawin@ucar.edu>
2021-11-05 12:33:36 -0600

    Github Issue #153 intermediary files: provide support for turning on/off the writing of intermediary file used by METviewer by setting dump_points_1 to True or False.

--- metplotpy/plots/performance_diagram/performance_diagram.py
+++ metplotpy/plots/performance_diagram/performance_diagram.py
@@ -263,7 +263,11 @@ class PerformanceDiagram(BasePlot):
 
         # "Dump" success ratio and PODY points to an output
         # file based on the output image filename (useful in debugging)
-        self.write_output_file()
+        if self.config_obj.dump_points_1 is True:
+            self.write_output_file()
+        else:
+            # needed by METviewer
+            self.write_output_file()
 
         for i, series in enumerate(self.series_list):
             # Don't generate the plot for this series if
