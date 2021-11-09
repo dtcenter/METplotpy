db8b980aa992d0e07a7932895ef8d1431cf03790
bikegeek <minnawin@ucar.edu>
2021-11-09 15:07:39 -0700

    Github Issue #153 updated assert

--- test/performance_diagram/test_performance_diagram.py
+++ test/performance_diagram/test_performance_diagram.py
@@ -1,31 +1,34 @@
-# !!!IMPORTANT!!!
-# activate conda environment in the testing subshell
-# Replace blenny_363 with your METplus Python 3.6.3
-# conda environment name
-# !!!!!!!!
-
-#!/usr/bin/env conda run -n blenny_363 python
-
-import pytest
 import os
-import performance_diagram as pd
 import sys
-sys.path.append('../..')
-from metplotpy.plots.performance_diagram import performance_diagram as pd
+# sys.path.append("/Users/minnawin/feature_153_intermed_files/METplotpy/metplotpy")
+sys.path.append("../../")
+import yaml
+import pytest
+import plots.util as util
+from plots.performance_diagram import performance_diagram as pd
+
+
 from metcalcpy.compare_images import CompareImages
+from plots.roc_diagram import roc_diagram as roc
 
 @pytest.fixture
 def setup():
     # Cleanup the plotfile and point1 output file from any previous run
-    cleanup()
+    # cleanup()
     # Set up the METPLOTPY_BASE so that met_plot.py will correctly find
     # the config directory containing all the default config files.
     os.environ['METPLOTPY_BASE'] = "../../metplotpy"
     custom_config_filename = "./custom_performance_diagram.yaml"
+    print("\n current directory: ", os.getcwd())
+    print("\ncustom config file: ", custom_config_filename, '\n')
 
     # Invoke the command to generate a Performance Diagram based on
     # the custom_performance_diagram.yaml custom config file.
+    # Retrieve the contents of the custom config file to over-ride
+    # or augment settings defined by the default config file.
+    # roc.main(custom_config_filename)
     pd.main(custom_config_filename)
+
 
 def cleanup():
     # remove the performance_diagram_expected.png and plot_20200317_151252.points1 files
@@ -42,10 +45,11 @@ def cleanup():
         pass
 
 
-@pytest.mark.parametrize("test_input,expected_bool",(["./performance_diagram_actual.png", True], ["./plot_20200317_151252.points1", True]))
+@pytest.mark.parametrize("test_input,expected_bool",(["./performance_diagram_actual.png", True], ["./plot_20200317_151252.points1", False]))
 def test_files_exist(setup, test_input, expected_bool):
     '''
-        Checking that the plot and data files are getting created
+        Checking that only the plot file is getting created and the
+         .point1 data file is not (dump_points_1 is 'False' in the test config file)
     '''
     assert os.path.isfile(test_input) == expected_bool
     cleanup()
@@ -67,4 +71,3 @@ def test_images_match(setup):
     comparison = CompareImages('./performance_diagram_expected.png',actual_file)
     assert comparison.mssim == 1
     cleanup()
-
