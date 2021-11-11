a78c1df3275ce3c29ec16b6f1416efbe07d32e91
bikegeek <minnawin@ucar.edu>
2021-11-11 16:08:51 -0700

    updates to test for Issue #153 intermediate files

--- test/performance_diagram/test_performance_diagram.py
+++ test/performance_diagram/test_performance_diagram.py
@@ -1,14 +1,9 @@
 import os
 import sys
 sys.path.append("../../")
-import yaml
 import pytest
-import plots.util as util
 from plots.performance_diagram import performance_diagram as pd
-
-
 from metcalcpy.compare_images import CompareImages
-from plots.roc_diagram import roc_diagram as roc
 
 @pytest.fixture
 def setup():
@@ -18,8 +13,8 @@ def setup():
     # the config directory containing all the default config files.
     os.environ['METPLOTPY_BASE'] = "../../metplotpy"
     custom_config_filename = "./custom_performance_diagram.yaml"
-    print("\n current directory: ", os.getcwd())
-    print("\ncustom config file: ", custom_config_filename, '\n')
+    # print("\n current directory: ", os.getcwd())
+    # print("\ncustom config file: ", custom_config_filename, '\n')
 
     # Invoke the command to generate a Performance Diagram based on
     # the custom_performance_diagram.yaml custom config file.
@@ -45,13 +40,82 @@ def cleanup():
 
 
 @pytest.mark.parametrize("test_input,expected_bool",(["./performance_diagram_actual.png", True], ["./plot_20200317_151252.points1", False]))
-def test_files_exist(setup, test_input, expected_bool):
+def test_plot_exists(setup, test_input, expected_bool):
     '''
         Checking that only the plot file is getting created and the
          .point1 data file is not (dump_points_1 is 'False' in the test config file)
     '''
+
     assert os.path.isfile(test_input) == expected_bool
     cleanup()
+
+@pytest.mark.parametrize("test_input,expected_bool",(["./performance_diagram_actual_points1.png", True], ["./intermed_files/plot_20200317_151252.points1", True]))
+def test_files_exist(test_input, expected_bool):
+    '''
+        Checking that only the plot file is getting created and the
+         .point1 data file is not (dump_points_1 is 'False' in the test config file)
+    '''
+
+    os.environ['METPLOTPY_BASE'] = "../../metplotpy"
+    custom_config_filename = "./custom_performance_diagram_points1.yaml"
+    try:
+       os.mkdir(os.path.join(os.getcwd(), 'intermed_files'))
+    except FileExistsError as e:
+        pass
+
+    # Invoke the command to generate a Performance Diagram based on
+    # the custom_performance_diagram.yaml custom config file.
+    # Retrieve the contents of the custom config file to over-ride
+    # or augment settings defined by the default config file.
+    # roc.main(custom_config_filename)
+    pd.main(custom_config_filename)
+
+    assert os.path.isfile(test_input) == expected_bool
+    try:
+        path = os.getcwd()
+        plot_file = 'performance_diagram_actual_points1.png'
+        points_file = './intermed_files/plot_20200317_151252.points1'
+        os.remove(os.path.join(path, plot_file))
+        os.remove(os.path.join(path, points_file))
+    except OSError as e:
+        # Typically when files have already been removed or
+        # don't exist.  Ignore.
+        pass
+
+
+@pytest.mark.parametrize("test_input,expected_bool",(["./performance_diagram_defaultpoints1.png", True], ["./plot_20200317_151252.points1", True]))
+def test_files_exist(test_input, expected_bool):
+    '''
+        Checking that only the plot file is getting created and the
+         .point1 data file is not (dump_points_1 is 'False' in the test config file)
+    '''
+
+    os.environ['METPLOTPY_BASE'] = "../../metplotpy"
+    custom_config_filename = "./custom_performance_diagram_defaultpoints1.yaml"
+    try:
+       os.mkdir(os.path.join(os.getcwd(), 'intermed_files'))
+    except FileExistsError as e:
+        pass
+
+    # Invoke the command to generate a Performance Diagram based on
+    # the custom_performance_diagram.yaml custom config file.
+    # Retrieve the contents of the custom config file to over-ride
+    # or augment settings defined by the default config file.
+    # roc.main(custom_config_filename)
+    pd.main(custom_config_filename)
+
+    assert os.path.isfile(test_input) == expected_bool
+    try:
+        path = os.getcwd()
+        plot_file = 'performance_diagram_defaultpoints1.png'
+        points_file = 'plot_20200317_151252.points1'
+        os.remove(os.path.join(path, plot_file))
+        os.remove(os.path.join(path, points_file))
+    except OSError as e:
+        # Typically when files have already been removed or
+        # don't exist.  Ignore.
+        pass
+
 
 
 def test_images_match(setup):
