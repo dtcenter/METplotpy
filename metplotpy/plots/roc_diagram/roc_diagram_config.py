b78b21be2cbc50a660eeef4bb234fae968c591a3
bikegeek <minnawin@ucar.edu>
2021-11-11 15:00:35 -0700

    Github Issue #153 Support for specifying location for storing the points1 file

--- metplotpy/plots/roc_diagram/roc_diagram_config.py
+++ metplotpy/plots/roc_diagram/roc_diagram_config.py
@@ -30,7 +30,11 @@ class ROCDiagramConfig(Config):
         self.create_html = self._get_bool('create_html')
 
         # Write (or not write) output points file provided by METviewer
-        self.dump_points = self._get_bool('dump_points_1')
+        self.dump_points_1 = self._get_bool('dump_points_1')
+
+        # Optional setting, indicates *where* to save the dump_points_1 file
+        # used by METviewer
+        self.points_path = self.get_config_value('points_path')
 
         # use this setting to determine the ordering of colors, lines, and markers
         self.series_ordering = self._get_series_order()
