c1a63a309c812fd680af16961e622a3eaf426371
bikegeek <minnawin@ucar.edu>
2021-11-11 21:11:43 -0700

    updated tests for Github issue #153

--- test/box/test_box.py
+++ test/box/test_box.py
@@ -60,3 +60,62 @@ def test_images_match(setup):
     comparison = CompareImages('./box_expected.png', './box.png')
     assert comparison.mssim == 1
     cleanup()
+
+
+@pytest.mark.parametrize("test_input, expected",
+                         (["./box_expected.png", True],["./box_points1.png", True],["./intermed_files/box.points1", True]))
+def test_points1_file_exist(test_input, expected):
+    """
+        Checking that the plot is created and points1 output files is created
+        where specified in the custom_box_points1.yaml file
+    """
+    os.environ['METPLOTPY_BASE'] = "../../metplotpy"
+    custom_config_filename = "custom_box_points1.yaml"
+    try:
+       os.mkdir(os.path.join(os.getcwd(), './intermed_files'))
+    except FileExistsError as e:
+        pass
+
+    # Invoke the command to generate a box plot based on
+    # the custom_box_points1.yaml custom config file.
+    box.main(custom_config_filename)
+    assert os.path.isfile(test_input) == expected
+    try:
+        path = os.getcwd()
+        plot_file = 'box_points1.png'
+        points_file_1 = 'box.points1'
+        subdir = os.path.join(path, './intermed_files')
+        os.remove(os.path.join(path, plot_file))
+        os.remove(os.path.join(subdir, points_file_1))
+    except OSError as e:
+        # Typically when files have already been removed or
+        # don't exist.  Ignore.
+        pass
+
+
+@pytest.mark.parametrize("test_input, expected",
+                         (["./box_defaultpoints1.png", True],["./box.points1", True]))
+def test_defaultpoints1_file_exist(test_input, expected):
+    """
+        Checking that the plot is created and points1 output files is created
+        in the default location (i.e. the current working dir, where the box.data file resides)
+    """
+    os.environ['METPLOTPY_BASE'] = "../../metplotpy"
+    custom_config_filename = "custom_box_defaultpoints1.yaml"
+
+    # Invoke the command to generate a box plot based on
+    # the custom_box_defaultpoints1.yaml custom config file.
+    box.main(custom_config_filename)
+    assert os.path.isfile(test_input) == expected
+
+    # remove the created plot and intermediate .points1 file
+    try:
+        path = os.getcwd()
+        plot_file = 'box_defaultpoints1.png'
+        points_file_1 = 'box.points1'
+        os.remove(os.path.join(path, plot_file))
+        os.remove(os.path.join(path, points_file_1))
+    except OSError as e:
+        # Typically when files have already been removed or
+        # don't exist.  Ignore.
+        pass
