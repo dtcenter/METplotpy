bb50d1e50a1c68b656a1b873224755f31fe7b681
bikegeek <minnawin@ucar.edu>
2021-12-16 18:37:43 -0700

    no tests are run, the minimal_histogram_2d.yaml file must have explicit paths defined for stat_input and plot_filename to run correctly

--- test/histogram_2d/test_histogram_2d.py
+++ test/histogram_2d/test_histogram_2d.py
@@ -12,39 +12,34 @@ def setup():
     # Set up the METPLOTPY_BASE so that met_plot.py will correctly find
     # the config directory containing all the default config files.
     os.environ['METPLOTPY_BASE'] = "../../metplotpy"
-    custom_config_filename = "./custom_histogram_2d.yaml"
+    custom_config_filename = "./minimal_histogram_2d.yaml"
     # print("\n current directory: ", os.getcwd())
     # print("\ncustom config file: ", custom_config_filename, '\n')
 
-    # Invoke the command to generate a Performance Diagram based on
-    # the test_custom_performance_diagram.yaml custom config file.
-    # Retrieve the contents of the custom config file to over-ride
-    # or augment settings defined by the default config file.
-    # roc.main(custom_config_filename)
+    # Invoke the command to generate a histogram_2d plot based on
+    # the settings in the "defaults" config file.
     h2d.main(custom_config_filename)
 
 
-def cleanup():
-    # remove the performance_diagram_expected.png and plot_20200317_151252.points1 files
-    # from any previous runs
-    try:
-        path = os.getcwd()
-        plot_file = '../../docs/Users_Guide/custom_tmp_z2_p500.png'
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!  IMPORTANT !!!!!!
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# change the stat_input and plot_filename to explicitly point to this directory before
+# running the test below because xarray cannot handle relative paths when reading in filenames
+# def test_plot_exists(setup):
+#     '''
+#         Checking that only the "defaults" plot file is getting created
+#
+#     '''
+#     test_input = "./tmp_z2_p500.png"
+#     assert os.path.exists(test_input)
+#     # cleanup
+#     try:
+#         path = os.getcwd()
+#         plot_file = 'tmp_z2_p500.png'
+#         os.remove(os.path.join(path, plot_file))
+#     except OSError as e:
+#         # Typically when files have already been removed or
+#         # don't exist.  Ignore.
+#         pass
 
-        os.remove(os.path.join(path, plot_file))
-        os.remove(os.path.join(path, points_file))
-    except OSError as e:
-        # Typically when files have already been removed or
-        # don't exist.  Ignore.
-        pass
-
-
-@pytest.mark.parametrize("test_input,expected_bool",(["./performance_diagram_actual.png", True], ["./plot_20200317_151252.points1", False]))
-def test_plot_exists(setup, test_input, expected_bool):
-    '''
-        Checking that only the plot file is getting created and the
-         .point1 data file is not (dump_points_1 is 'False' in the test config file)
-    '''
-
-    assert os.path.isfile(test_input) == expected_bool
-    cleanup()
