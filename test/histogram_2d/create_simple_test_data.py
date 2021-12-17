7e87d26c7e6ca3e23426186828271e271c81c127
bikegeek <minnawin@ucar.edu>
2021-12-16 18:01:56 -0700

    files needed for testing and for user instructions

---
+++ test/histogram_2d/create_simple_test_data.py
@@ -0,0 +1,26 @@
+import numpy as np
+import xarray as xr
+
+
+def create_test_data(filename_in):
+    """
+
+    :param filename_in: the full filename (path + filename) of the test data
+    :return:
+    """
+    print('Creating test data')
+    nx, ny = 21, 21
+    x_coord = np.linspace(-2, 2, nx)
+    y_coord = np.linspace(-2, 2, ny)
+    x_mesh, y_mesh = np.meshgrid(x_coord, y_coord, indexing='ij')
+    f_mesh = 4 - np.square(x_mesh - y_mesh + 1)
+    f_mesh = np.clip(f_mesh, 0, 4)
+    f_array = xr.DataArray(f_mesh,
+                           dims=['x', 'y'],
+                           coords={'x': x_coord, 'y': y_coord})
+    ds = xr.Dataset({'hist_x_y': f_array})
+    ds.to_netcdf(filename_in)
+
+if __name__ == "__main__":
+    test_data_filename = "./test_histogram_2d_data.nc"
+    create_test_data(test_data_filename)
