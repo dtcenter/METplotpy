a54bd7370955dce17e7a073fbad51e0e8ec1b1f7
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-02-15 13:23:32 -0700

    added another pytest for the import commands used in another METplus use case

--- test/import/test_import.py
+++ test/import/test_import.py
@@ -2,6 +2,16 @@
 
 import pytest
 
+# used in METplus use case: model_applications/medium_range/UserScript_fcstGEFS_Difficulty_Index
+def test_import_difficulty_index():
+    try:
+        import metplotpy.plots.difficulty_index.mycolormaps
+        from metplotpy.plots.difficulty_index.plot_difficulty_index import plot_field
+    except:
+        assert False
+    assert True
+
+# used in METplus use case: model_applications/s2s/UserScript_obsPrecip_obsOnly_Hovmoeller
 def test_import_hovmoeller():
     try:
         import metplotpy.plots.hovmoeller.hovmoeller
