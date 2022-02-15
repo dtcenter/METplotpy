9f0bb31d273f7b55e801d9dd1bf6665d3dc68fe3
ahijevyc <ahijevyc@ucar.edu>
2022-02-15 16:58:09 -0700

    rename cnvgwd->congwd, assign name of lat and lon variables

--- metplotpy/contributed/fv3_physics_tend/fv3.py
+++ metplotpy/contributed/fv3_physics_tend/fv3.py
@@ -1,8 +1,8 @@
 tendencies = dict(
-tmp = ['dt3dt_cnvgwd','dt3dt_deepcnv','dt3dt_lw','dt3dt_mp','dt3dt_orogwd','dt3dt_pbl','dt3dt_rdamp','dt3dt_shalcnv','dt3dt_sw','dt3dt_nophys'],
+tmp = ['dt3dt_congwd','dt3dt_deepcnv','dt3dt_lw','dt3dt_mp','dt3dt_orogwd','dt3dt_pbl','dt3dt_rdamp','dt3dt_shalcnv','dt3dt_sw','dt3dt_nophys'],
 spfh= ['dq3dt_deepcnv','dq3dt_mp','dq3dt_pbl','dq3dt_shalcnv','dq3dt_nophys'],
-ugrd= ['du3dt_cnvgwd','du3dt_deepcnv','du3dt_orogwd','du3dt_pbl','du3dt_rdamp','du3dt_shalcnv','du3dt_nophys'],
-vgrd= ['dv3dt_cnvgwd','dv3dt_deepcnv','dv3dt_orogwd','dv3dt_pbl','dv3dt_rdamp','dv3dt_shalcnv','dv3dt_nophys']
+ugrd= ['du3dt_congwd','du3dt_deepcnv','du3dt_orogwd','du3dt_pbl','du3dt_rdamp','du3dt_shalcnv','du3dt_nophys'],
+vgrd= ['dv3dt_congwd','dv3dt_deepcnv','dv3dt_orogwd','dv3dt_pbl','dv3dt_rdamp','dv3dt_shalcnv','dv3dt_nophys']
 )
 import cartopy.io.shapereader as shpreader
 import datetime
@@ -12,6 +12,11 @@ import os
 import pandas as pd
 import pdb
 from shapely.geometry import Point, multipolygon
+
+
+lon_name = "grid_lont"
+lat_name = "grid_latt"
+
 
 def pts_in_shp(lats, lons, shp, debug=False):
     # Map longitude to -180 to +180 range
