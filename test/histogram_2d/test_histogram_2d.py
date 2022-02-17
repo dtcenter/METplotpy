02d0c08e8d4969a1e63fa5791697feea4661c7dc
bikegeek <minnawin@ucar.edu>
2022-02-17 11:05:59 -0700

    only a test fixture in this file, but updated the METPLOTPY_BASE to reflect changes to base_plot.py

--- test/histogram_2d/test_histogram_2d.py
+++ test/histogram_2d/test_histogram_2d.py
@@ -11,7 +11,7 @@ def setup():
     # cleanup()
     # Set up the METPLOTPY_BASE so that met_plot.py will correctly find
     # the config directory containing all the default config files.
-    os.environ['METPLOTPY_BASE'] = "../../metplotpy"
+    os.environ['METPLOTPY_BASE'] = "../../"
     custom_config_filename = "./minimal_histogram_2d.yaml"
     # print("\n current directory: ", os.getcwd())
     # print("\ncustom config file: ", custom_config_filename, '\n')
