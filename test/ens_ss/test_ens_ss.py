72de5954fdcc4ba7d2a0ad303e6aab8b3cf99740
bikegeek <minnawin@ucar.edu>
2022-02-17 09:58:28 -0700

    issue #140 update test to reflect change to base_plot.py

--- test/ens_ss/test_ens_ss.py
+++ test/ens_ss/test_ens_ss.py
@@ -18,7 +18,7 @@ def setup():
     cleanup()
     # Set up the METPLOTPY_BASE so that met_plot.py will correctly find
     # the config directory containing all the default config files.
-    os.environ['METPLOTPY_BASE'] = "../../metplotpy"
+    os.environ['METPLOTPY_BASE'] = "../../"
     custom_config_filename = "custom_ens_ss.yaml"
 
     # Invoke the command to generate a Bar plot based on
@@ -72,7 +72,7 @@ def test_files_exist(test_input, expected):
         os.mkdir(os.path.join(os.getcwd(), './intermed_files'))
     except FileExistsError as e:
         pass
-    os.environ['METPLOTPY_BASE'] = "../../metplotpy"
+    os.environ['METPLOTPY_BASE'] = "../../"
     custom_config_filename = "custom2_ens_ss.yaml"
 
     # Invoke the command to generate a Bar plot based on
