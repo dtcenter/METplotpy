15c55bbb725109e9fa8ba038019a5ba4f451c7f7
bikegeek <minnawin@ucar.edu>
2022-02-24 11:22:07 -0700

    fixed import statement, looks like some of it was removed

--- metplotpy/plots/wind_rose/wind_rose.py
+++ metplotpy/plots/wind_rose/wind_rose.py
@@ -17,7 +17,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 from metplotpy.plots.base_plot import BasePlot
-from .wind_rose_config import WindRoseConfig
+from plots.wind_rose.wind_rose_config import WindRoseConfig
 from metplotpy.plots.constants import PLOTLY_AXIS_LINE_COLOR, PLOTLY_AXIS_LINE_WIDTH, PLOTLY_PAPER_BGCOOR
 from metplotpy.plots import util
 
