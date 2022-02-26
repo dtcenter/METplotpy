ea1b87e0391c6650de11a9f957e149bb33e32376
Minna Win <minnawin@seneca.rap.ucar.edu>
2022-02-25 17:44:13 -0700

    #204 added copyright to python and shell scripts

--- metplotpy/contributed/tc_rmw/test_plot_cross_section.sh
+++ metplotpy/contributed/tc_rmw/test_plot_cross_section.sh
@@ -1,3 +1,10 @@
+# ============================*
+# ** Copyright UCAR (c) 2020
+# ** University Corporation for Atmospheric Research (UCAR)
+# ** National Center for Atmospheric Research (NCAR)
+# ** Research Applications Lab (RAL)
+# ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
+# ============================*
 
 export datadir=/path/to/vertically-interpolated-input-data
 export plotdir=/path/to/output/plots
