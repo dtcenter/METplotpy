1fa8555c3622f17b3ee72f240488d1411a72fe29
ahijevyc <ahijevyc@ucar.edu>
2022-03-11 10:03:51 -0700

    add mp to ugrd and vgrd

--- metplotpy/contributed/fv3_physics_tend/fv3.py
+++ metplotpy/contributed/fv3_physics_tend/fv3.py
@@ -1,8 +1,8 @@
 tendencies = dict(
 tmp = ['dt3dt_congwd','dt3dt_deepcnv','dt3dt_lw','dt3dt_mp','dt3dt_orogwd','dt3dt_pbl','dt3dt_rdamp','dt3dt_shalcnv','dt3dt_sw','dt3dt_nophys'],
 spfh= ['dq3dt_deepcnv','dq3dt_mp','dq3dt_pbl','dq3dt_shalcnv','dq3dt_nophys'],
-ugrd= ['du3dt_congwd','du3dt_deepcnv','du3dt_orogwd','du3dt_pbl','du3dt_rdamp','du3dt_shalcnv','du3dt_nophys'],
-vgrd= ['dv3dt_congwd','dv3dt_deepcnv','dv3dt_orogwd','dv3dt_pbl','dv3dt_rdamp','dv3dt_shalcnv','dv3dt_nophys']
+ugrd= ['du3dt_congwd','du3dt_deepcnv','du3dt_mp','du3dt_orogwd','du3dt_pbl','du3dt_rdamp','du3dt_shalcnv','du3dt_nophys'],
+vgrd= ['dv3dt_congwd','dv3dt_deepcnv','dv3dt_mp','dv3dt_orogwd','dv3dt_pbl','dv3dt_rdamp','dv3dt_shalcnv','dv3dt_nophys']
 )
 import cartopy.io.shapereader as shpreader
 import datetime
