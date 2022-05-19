aa36fd0264f74b21e7b11c8b02d4bf3c44373962
bikegeek <minnawin@ucar.edu>
2022-05-19 09:45:47 -0600

    Issue #217 fixed retrieving of start_from_zero setting

--- metplotpy/plots/line/line.py
+++ metplotpy/plots/line/line.py
@@ -246,7 +246,7 @@ class Line(BasePlot):
         self._add_x2axis(n_stats)
 
         # Allow plots to start from the y=0 line if set in the config file
-        if self.config_obj.get_config_value('start_from_zero'):
+        if self.config_obj.start_from_zero is True:
             self.figure.update_xaxes(range=[0, len(x_points_index) - 1])
 
     def _draw_series(self, series: Series, x_points_index_adj: Union[list, None] = None) -> None:
