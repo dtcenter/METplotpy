2d2c03c2c1be86a83383a4740d20c72cf362f1e7
bikegeek <minnawin@ucar.edu>
2022-08-19 14:48:32 -0600

    use the correct yaml config file in the command

--- test/fv3_physics_tend/runner_planview.py
+++ test/fv3_physics_tend/runner_planview.py
@@ -21,7 +21,7 @@ def run_help(config_file):
 def run_example(config_file):
     '''Run the example in the user's guide'''
     config = open_config(config_file)
-    command_str = "python " +  config['source_dir'] + "/planview_fv3.py " + " ./runner_planview.yaml " +  config['history_file'] + " " + config['grid_file'] + " tmp pbl -p 500 -t 6 -v 20190504T21 --nofineprint "
+    command_str = "python " +  config['source_dir'] + "/planview_fv3.py " + " ./fv3_physics_tend_defaults.yaml " +  config['history_file'] + " " + config['grid_file'] + " tmp pbl -p 500 -t 6 -v 20190504T21 --nofineprint "
     print("command string: ", command_str)
     os.system(command_str)
 
