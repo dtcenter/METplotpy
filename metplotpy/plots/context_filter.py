ca50121ce8a2cdcb1724e94478b1fd77a404a668
bikegeek <minnawin@ucar.edu>
2023-05-11 15:51:38 -0600

    Internal Issue #47 added context filter to support adding user id to log file

---
+++ metplotpy/plots/context_filter.py
@@ -0,0 +1,22 @@
+import logging
+import getpass
+
+class ContextFilter(logging.Filter):
+    '''
+        This is a filter which injects contextual information into the log.
+        The contextual information is the user name but can also be other useful
+        information about the user such as the IP address from which this code is
+        being run.
+
+    '''
+
+    def filter(self, record):
+       '''
+         Args:
+           record: data struction containing the user information.
+         Returns: True when the record is created
+       '''
+
+       # Retrieve the user id of the user running the code.
+       record.user = getpass.getuser()
+       return True
