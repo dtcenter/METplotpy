0b55ced3b8755a0d9c1fbfa52b3198c39c80ae4e
bikegeek <minnawin@ucar.edu>
2023-10-23 17:30:55 -0600

    Issue #391 replace tests for create_query with tests for filter_by_vars

--- test/util/test_util.py
+++ test/util/test_util.py
@@ -75,3 +75,78 @@ def test_sort_threshold_values_floats():
     for idx, sorted in enumerate(sorted_list):
         if sorted != expected_list[idx]:
             assert False
+
+def test_is_thresh():
+    """ Verify that the regular expression used to test
+        for column labels that are threshold-types (i.e. fcst_thresh, obs_thresh,
+        cov_thresh).
+
+    """
+    no_thresh_cols = ['fcst_lead', 'vx_mask', 'fcst_init_beg']
+    thresh_cols = ['fcst_thresh', 'obs_thresh', 'cov_thresh', 'xyz_thresh_abc']
+    mixed_cols = ['fcst_lead', 'obtype', 'obs_thresh', 'vx_mask', 'fcst_thresh']
+
+    counter = 0
+    expected_none = len(no_thresh_cols)
+    for cur_col in no_thresh_cols:
+       if not util.is_thresh_column(cur_col):
+           counter += 1
+
+    if counter == expected_none:
+        assert True
+
+    counter = 0
+    expected_all = len(thresh_cols)
+    for cur_col in thresh_cols:
+        if util.is_thresh_column(cur_col):
+            counter +=1
+    if counter == expected_all:
+        assert True
+    else:
+        assert False
+
+    expected_found = 2
+    counter = 0
+    for cur_col in mixed_cols:
+        if util.is_thresh_column(cur_col):
+            counter +=1
+
+    if counter == expected_found:
+        assert True
+    else:
+        assert False
+
+
+def test_filter_by_fixed_vars():
+    """
+       Verify that the expected query string is generated and the query
+       results are what are expected.
+
+
+    """
+
+settings_dict = {'fcst_thresh': ['NA', '>0.0', '>=0.254'], 'obtype': ['CCPA'],
+                 'vx_mask': ['CONUS'], 'fcst_init_beg': ['2023-06-24 12:00:00']}
+
+expected_query_list = ["fcst_thresh.isnull() | fcst_thresh in ( '>0.0', '>=0.254') ",
+                  "obtype in ('CCPA') ",
+                  "vx_mask in ('CONUS')",
+                  "fcst_init_beg in ('2023-06-24 12:00:00')"]
+
+input_df = pd.read_csv("./full_thresh_data.txt", sep='\t')
+filtered_df = util.filter_by_fixed_vars(input_df, settings_dict)
+
+expected_df = input_df.copy(deep=True)
+
+for qry in expected_query_list:
+    intermed_df = expected_df.query(qry)
+    expected_df = intermed_df.copy(deep=True)
+
+assert filtered_df.shape == expected_df.shape
+
+
+filtered_list = filtered_df['fcst_thresh'].to_list()
+expected_list = expected_df['fcst_thresh'].to_list()
+
+for filtered in filtered_list:
+   assert filtered in expected_list
