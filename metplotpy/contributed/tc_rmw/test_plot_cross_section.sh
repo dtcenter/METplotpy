bee14019030886c22e48c2166b43c3b78427603f
bikegeek <minnawin@ucar.edu>
2024-05-07 16:53:07 -0600

    Added logging settings with description

--- metplotpy/contributed/tc_rmw/test_plot_cross_section.sh
+++ metplotpy/contributed/tc_rmw/test_plot_cross_section.sh
@@ -6,13 +6,18 @@
 # ** P.O.Box 3000, Boulder, Colorado, 80307-3000, USA
 # ============================*
 
-export datadir=/path/to/vertically-interpolated-input-data
-export plotdir=/path/to/output/plots
-export filename=tc_rmw_example_vertical_interp.nc
-export configfile=/path/to/configuration-file
+export datadir=/Users/minnawin/AF_STIG/feature_57_METplotpy_logging/METplotpy/metplotpy/contributed/tc_rmw/Data
+export plotdir=/Users/minnawin/AF_STIG/feature_57_METplotpy_logging/METplotpy/metplotpy/contributed/tc_rmw/plots
+export filename=vertically_interpolated.nc
+export configfile=/Users/minnawin/AF_STIG/feature_57_METplotpy_logging/METplotpy/metplotpy/contributed/tc_rmw/plot_cross_section.yaml
+# Default is set to INFO in code, set to any other value here and add to the arguments
+# in the call to the plot_cross_section.py below
+export loglevel="ERROR"
+
 
 python plot_cross_section.py \
     --datadir=$datadir \
     --plotdir=$plotdir \
     --filename=$filename \
-    --config=$configfile
+    --config=$configfile \
+#    --loglevel=$loglevel
