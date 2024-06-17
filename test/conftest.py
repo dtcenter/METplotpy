19d7ddb21ce8995d0961badc14417b866535ab49
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2024-06-17 12:23:07 -0600

    add prints to help debugging if test fails

--- test/conftest.py
+++ test/conftest.py
@@ -6,6 +6,7 @@ import shutil
 @pytest.fixture
 def setup_env():
     def set_environ(test_dir):
+        print("Setting up environment")
         os.environ['METPLOTPY_BASE'] = f"{test_dir}/../../"
         os.environ['TEST_DIR'] = test_dir
     return set_environ
@@ -14,6 +15,7 @@ def setup_env():
 @pytest.fixture()
 def remove_files():
     def remove_the_files(test_dir, file_list):
+        print("Removing the files")
         # loop over list of files under test_dir and remove them
         for file in file_list:
             try:
@@ -22,6 +24,7 @@ def remove_files():
                 pass
 
         # also remove intermed_files directory if it exists
+        print("Removing intermed_files directory if it exists")
         try:
             shutil.rmtree(f"{test_dir}/intermed_files")
         except FileNotFoundError:
