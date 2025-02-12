8000b0765c90fdd1e0242aaba5dfce229ac6c566
bikegeek <minnawin@ucar.edu>
2025-02-12 11:08:41 -0700

    update to test as result of update to Python 3.12

--- test/scatter/test_scatter.py
+++ test/scatter/test_scatter.py
@@ -5,6 +5,8 @@ from metplotpy.plots import util
 def test_scatter(assert_json_equal):
     default_conf_filename = "scatter_defaults.yaml"
     scat = scatter.Scatter(util.get_params("custom_scatter.yaml"),default_conf_filename)
+    # Run the following when there is a change in the plot
+    # scat.figure.write_json('custom_scatter_expected.json')
     assert_json_equal(scat.figure, "custom_scatter_expected.json")
 
 
