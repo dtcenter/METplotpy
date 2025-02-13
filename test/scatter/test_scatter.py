37d00ae5d6619a4d625e587816a56d23b1eca3d4
bikegeek <minnawin@ucar.edu>
2025-02-12 17:05:27 -0700

    use correct decorator for skipping test

--- test/scatter/test_scatter.py
+++ test/scatter/test_scatter.py
@@ -3,7 +3,7 @@ import pytest
 from metplotpy.plots.scatter import scatter
 from metplotpy.plots import util
 
-@pytest.skip("plotly generates an html plot-this test cannot be run in a GHA")
+@pytest.mark.skip("plotly generates an html plot-this test cannot be run in a GHA")
 def test_scatter(assert_json_equal):
     default_conf_filename = "scatter_defaults.yaml"
     scat = scatter.Scatter(util.get_params("custom_scatter.yaml"),default_conf_filename)
