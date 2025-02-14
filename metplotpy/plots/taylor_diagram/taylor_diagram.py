5f8e0bd9e5a7b75ad87c0ab882123d9abb4d3be5
bikegeek <minnawin@ucar.edu>
2025-02-14 10:06:36 -0700

    Address MatplotlibDeprecation (in 3.9) to set apply_theta_transform to False.  There are no discernable differences to plots generated with pre 3.9 versions.

--- metplotpy/plots/taylor_diagram/taylor_diagram.py
+++ metplotpy/plots/taylor_diagram/taylor_diagram.py
@@ -24,6 +24,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 from matplotlib.projections import PolarAxes
+from matplotlib.transforms import Affine2D
 import mpl_toolkits.axisartist.floating_axes as fa
 import mpl_toolkits.axisartist.grid_finder as gf
 import numpy as np
@@ -172,7 +173,7 @@ class TaylorDiagram(BasePlot):
 
         # in Matplotlib 3.10, the default for apply_theta_transforms=True has been deprecated, explicitly set this
         # to True
-        tr = PolarAxes.PolarTransform(apply_theta_transforms=True)
+        tr = PolarAxes.PolarTransform(apply_theta_transforms=False)
 
         # Correlation labels
         rlocs = np.array([0, 0.2, 0.4, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 1])
