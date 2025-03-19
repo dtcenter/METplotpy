61869cf4133e9b7d9f537c4de065e481eccc47e1
bikegeek <minnawin@ucar.edu>
2025-03-19 16:53:13 -0600

    Updates to enable reusing the same config file and setup

--- test/fv3_physics_tend/conftest.py
+++ test/fv3_physics_tend/conftest.py
@@ -6,14 +6,14 @@ import yaml
 cwd = os.path.dirname(__file__)
 
 @pytest.fixture
-def setup_planview(autouse=True) -> dict:
+def setup_physics_tendency_test(autouse=True) -> dict:
     """
        Fixture for setting up the planview tests by
-       reading in the planview.yaml file and creating a
+       reading in the physics_tendency_test.yaml file and creating a
        dictionary representation of the settings.
     """
     # open and read the test config file
-    with open(os.path.join(cwd, "planview.yaml"), 'r') as stream:
+    with open(os.path.join(cwd, "physics_tendency_test.yaml"), 'r') as stream:
         try:
             return yaml.load(stream, Loader=yaml.FullLoader)
         except yaml.YAMLError as exc:
--- test/fv3_physics_tend/test_planview.py
+++ test/fv3_physics_tend/test_planview.py
@@ -1,9 +1,7 @@
 import pytest
 import os
-
 import xarray
 import yaml
-import xarray as xr
 import sys
 import metplotpy.contributed.fv3_physics_tend.physics_tend as pt
 import metplotpy.contributed.fv3_physics_tend.planview_fv3 as pv
@@ -24,13 +22,13 @@ def cleanup(generated_plot):
 @pytest.mark.skip("skip because dataset is too large")
 @pytest.mark.parametrize("test_config, expected", ([f"{cwd}/tmp_500hPa.yaml", "tmp_500hPa.png"],
                                                   [f"{cwd}/tmp_pbl.yaml", "tmp_pbl.png"]))
-def test_planview_plot_created(setup_planview, test_config, expected):
+def test_planview_plot_created(setup_physics_tendency_test, test_config, expected):
     """
     Test if each planview plot file is created
     """
 
     test_config_obj = create_config_from_filename(test_config)
-    ds:xarray.Dataset = create_phys_tendency_dataset(setup_planview, test_config_obj)
+    ds:xarray.Dataset = create_phys_tendency_dataset(setup_physics_tendency_test, test_config_obj)
 
     # Generate the planview plot based on the settings from the test config files
     planview_obj = pv.planview(test_config_obj, ds, pfull=test_config_obj['pfull'], robust=test_config_obj['robust'],
@@ -46,13 +44,13 @@ def test_planview_plot_created(setup_planview, test_config, expected):
 @pytest.mark.skip("skip because dataset is too large")
 @pytest.mark.parametrize("test_config, expected", ([f"{cwd}/tmp_500hPa.yaml", "tmp_500hPa.png"],
                                                   [f"{cwd}/tmp_pbl.yaml", "tmp_pbl.png"]))
-def test_planview_plot_not_empty(setup_planview, test_config, expected):
+def test_planview_plot_not_empty(setup_physics_tendency_test, test_config, expected):
     """
     Test if each planview plot file is NOT empty
     """
 
     test_config_obj = create_config_from_filename(test_config)
-    ds:xarray.Dataset = create_phys_tendency_dataset(setup_planview, test_config_obj)
+    ds:xarray.Dataset = create_phys_tendency_dataset(setup_physics_tendency_test, test_config_obj)
 
     # Generate the planview plot based on the settings from the test config files
     planview_obj = pv.planview(test_config_obj, ds, pfull=test_config_obj['pfull'], robust=test_config_obj['robust'],
@@ -68,12 +66,12 @@ def test_planview_plot_not_empty(setup_planview, test_config, expected):
     assert os.stat(expected_plot).st_size > 300000
     cleanup(expected_plot)
 
-def create_phys_tendency_dataset(setup_planview:dict, test_config:dict) -> xarray.Dataset:
+def create_phys_tendency_dataset(setup_physics_tendency_test:dict, test_config:dict) -> xarray.Dataset:
     """
        Generate the dataset necessary for generating the planview plot
     """
 
-    return pt.prepare_ds(test_config, setup_planview['history_file'], setup_planview['grid_file'])
+    return pt.prepare_ds(test_config, setup_physics_tendency_test['history_file'], setup_physics_tendency_test['grid_file'])
 
 
 
