4c67e94d6c3457062029a2a0160a9bd1b1f9c403
bikegeek <minnawin@ucar.edu>
2025-03-19 18:10:46 -0600

    Moved functionality from test_planview into conftest for re-use by other tests

--- test/fv3_physics_tend/conftest.py
+++ test/fv3_physics_tend/conftest.py
@@ -1,22 +1,52 @@
 import pytest
 import os
 import sys
+import xarray
 import yaml
 
 cwd = os.path.dirname(__file__)
+FILE_INFO_CONFIG = "physics_tendency_test.yaml"
+
 
 @pytest.fixture
 def setup_physics_tendency_test(autouse=True) -> dict:
     """
-       Fixture for setting up the planview, vertical profile, and vertical cross-section tests by
-       reading in the physics_tendency_test.yaml file and creating a
-       dictionary representation of those settings.
+      Reads in the physics_tendency_test.yaml file and creating a
+       dictionary representation of those settings which indicate where the
+       grid and history files are located (and the data directory location, where additional
+       files can be found).
     """
-    # open and read the test config file
-    with open(os.path.join(cwd, "physics_tendency_test.yaml"), 'r') as stream:
+    # open and read the config file that contains file and data location
+    with open(os.path.join(cwd, FILE_INFO_CONFIG), 'r') as stream:
         try:
             return yaml.load(stream, Loader=yaml.FullLoader)
         except yaml.YAMLError as exc:
             print(exc)
             sys.exit(1)
 
+
+def create_config_from_filename(config_name) -> dict:
+    """
+       Reads in the full filepath config filename (YAML) and creates
+       a configuration object (dictionary representation of the settings)
+    """
+    with open(config_name, 'r') as stream:
+        try:
+            return yaml.load(stream, Loader=yaml.FullLoader)
+        except yaml.YAMLError as exc:
+            print(exc)
+            sys.exit(1)
+
+def create_physics_tendency_dataset(plot_config_file: str) -> xarray.Dataset:
+    """
+       Generate the dataset necessary for generating the planview, vertical cross-section, or vertical profile plot
+
+       Input:
+         plot_config_file - the filename of the YAML config file used to generate a plot
+
+       Returns:
+            an xarray dataset created by the physics tendency module
+    """
+    plot_test_config = create_config_from_filename(plot_config_file)
+    return pt.prepare_ds(plot_test_config, plot_test_config['history_file'],
+                         plot_test_config['grid_file'])
