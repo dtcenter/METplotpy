13c85b38a4eb7c39dc6a3332783bcc24bbea1e75
bikegeek <minnawin@ucar.edu>
2025-03-19 18:14:40 -0600

    updated to use functions in conftest.py

--- test/fv3_physics_tend/test_planview.py
+++ test/fv3_physics_tend/test_planview.py
@@ -1,10 +1,9 @@
 import pytest
 import os
 import xarray
-import yaml
-import sys
-import metplotpy.contributed.fv3_physics_tend.physics_tend as pt
+from conftest import create_config_from_filename, create_physics_tendency_dataset
 import metplotpy.contributed.fv3_physics_tend.planview_fv3 as pv
+import metplotpy.contributed.fv3_physics_tend.physics_tend as pt
 
 cwd = os.path.dirname(__file__)
 
@@ -26,16 +25,16 @@ def test_planview_plot_created(setup_physics_tendency_test, test_config, expecte
     """
     Test if each planview plot file is created
     """
-
-    test_config_obj = create_config_from_filename(test_config)
-    ds:xarray.Dataset = create_phys_tendency_dataset(setup_physics_tendency_test, test_config_obj)
+    plot_config_obj = create_config_from_filename(test_config)
+    ds = pt.prepare_ds(plot_config_obj, setup_physics_tendency_test['history_file'],
+                         setup_physics_tendency_test['grid_file'])
 
     # Generate the planview plot based on the settings from the test config files
-    planview_obj = pv.planview(test_config_obj, ds, pfull=test_config_obj['pfull'], robust=test_config_obj['robust'],
-                                     shp=test_config_obj['shp'], twindow=test_config_obj['twindow'],
-                                     validtime=test_config_obj['validtime'])
-    ofile = pv.default_ofile(test_config_obj)
-    planview_obj.fig.savefig(ofile, dpi=test_config_obj["dpi"])
+    planview_obj = pv.planview(plot_config_obj, ds, pfull=plot_config_obj['pfull'], robust=plot_config_obj['robust'],
+                                     shp=plot_config_obj['shp'], twindow=plot_config_obj['twindow'],
+                                     validtime=plot_config_obj['validtime'])
+    ofile = pv.default_ofile(plot_config_obj)
+    planview_obj.fig.savefig(ofile, dpi=plot_config_obj["dpi"])
 
     expected_plot = os.path.join(f"{cwd}", expected)
     assert os.path.exists(expected_plot)
@@ -49,41 +48,32 @@ def test_planview_plot_not_empty(setup_physics_tendency_test, test_config, expec
     Test if each planview plot file is NOT empty
     """
 
-    test_config_obj = create_config_from_filename(test_config)
-    ds:xarray.Dataset = create_phys_tendency_dataset(setup_physics_tendency_test, test_config_obj)
+    plot_config_obj = create_config_from_filename(test_config)
+    ds = pt.prepare_ds(plot_config_obj, setup_physics_tendency_test['history_file'],
+                         setup_physics_tendency_test['grid_file'])
 
     # Generate the planview plot based on the settings from the test config files
-    planview_obj = pv.planview(test_config_obj, ds, pfull=test_config_obj['pfull'], robust=test_config_obj['robust'],
-                                     shp=test_config_obj['shp'], twindow=test_config_obj['twindow'],
-                                     validtime=test_config_obj['validtime'])
-    ofile = pv.default_ofile(test_config_obj)
-    planview_obj.fig.savefig(ofile, dpi=test_config_obj["dpi"])
+    planview_obj = pv.planview(plot_config_obj, ds, pfull=plot_config_obj['pfull'], robust=plot_config_obj['robust'],
+                                     shp=plot_config_obj['shp'], twindow=plot_config_obj['twindow'],
+                                     validtime=plot_config_obj['validtime'])
+    ofile = pv.default_ofile(plot_config_obj)
+    planview_obj.fig.savefig(ofile, dpi=plot_config_obj["dpi"])
 
     expected_plot = os.path.join(f"{cwd}", expected)
+    assert os.path.exists(expected_plot)
+
 
     # Check for empty plot
     # Use a 3 kb size as lowest expected size of the plots to be generated
     assert os.stat(expected_plot).st_size > 300000
     cleanup(expected_plot)
 
-def create_phys_tendency_dataset(setup_physics_tendency_test:dict, test_config:dict) -> xarray.Dataset:
-    """
-       Generate the dataset necessary for generating the planview plot
-    """
-
-    return pt.prepare_ds(test_config, setup_physics_tendency_test['history_file'], setup_physics_tendency_test['grid_file'])
 
 
 
-def create_config_from_filename(config_name) -> dict:
-    """
-       Reads in the full filepath config filename (YAML) and creates
-       a configuration object (dictionary representation of the settings)
-    """
-    with open(config_name, 'r') as stream:
-        try:
-            return yaml.load(stream, Loader=yaml.FullLoader)
-        except yaml.YAMLError as exc:
-            print(exc)
-            sys.exit(1)
-
+# def create_phys_tendency_dataset(setup_physics_tendency_test:dict, test_config:dict) -> xarray.Dataset:
+#     """
+#        Generate the dataset necessary for generating the planview, vertical cross-section, or vertical profile plot
+#     """
+#
+#     return pt.prepare_ds(test_config, setup_physics_tendency_test['history_file'], setup_physics_tendency_test['grid_file'])
