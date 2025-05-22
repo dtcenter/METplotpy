5515de388a97298fbb85413e3e3139a410578245
MWin <3753118+bikegeek@users.noreply.github.com>
2025-05-22 10:06:08 -0600

    Feature 508 v3.1.0 rc1 (#510)
    * Updated version and date for rc1
    
    * Updates for RC1
    
    * Adding None under bugfixes
    
    * Removing two issues for documentation that were included in beta2
    
    * Align None under Repository, build, and test
    
    ---------
    
    Co-authored-by: Julie Prestopnik <jpresto@ucar.edu>

--- docs/conf.py
+++ docs/conf.py
@@ -25,12 +25,12 @@ project = 'METplotpy'
 copyright = '2025, NSF NCAR'
 author = 'UCAR/NSF NCAR, NOAA, CSU/CIRA, and CU/CIRES'
 author_list = 'Fisher, H., D. Adriaansen, C. Kalb, D. Fillmore, T. Jensen, L. Goodrich, M. Win-Gildenmeister and T. Burek'
-version = '3.1.0-rc1-dev'
+version = '3.1.0-rc1'
 verinfo = version
 release = f'{version}'
 release_year = '2025'
 
-release_date = f'{release_year}-03-31'
+release_date = f'{release_year}-05-22'
 
 copyright = f'{release_year}, {author}'
 
