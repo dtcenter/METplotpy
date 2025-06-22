76160ed57f9d1d0663e5fc35f819368941b2330b
bikegeek <minnawin@ucar.edu>
2025-06-22 16:47:19 -0600

    Pass in the stat name to the tcmpr line mean constructor

--- metplotpy/plots/tcmpr_plots/line/mean/tcmpr_line_mean.py
+++ metplotpy/plots/tcmpr_plots/line/mean/tcmpr_line_mean.py
@@ -97,8 +97,8 @@ class TcmprLineMean(TcmprLine):
                 oper = name[2]
                 name[:] = [(s + ' ' + stat_name) if ' ' not in s else s for s in name[:2]]
                 name.append(oper)
-
-                series_obj = TcmprSeriesLineMean(self.config_obj, num_series_y1 + i, input_data, series_list, name)
+                
+                series_obj = TcmprSeriesLineMean(self.config_obj, num_series_y1 + i, input_data, series_list, name, stat_name)
                 series_list.append(series_obj)
 
         # reorder series
