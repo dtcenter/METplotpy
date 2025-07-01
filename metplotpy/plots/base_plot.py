ea2f281831122abbf0c730b5f1964d397c358c16
bikegeek <minnawin@ucar.edu>
2025-07-01 08:44:19 -0600

    add call to install chrome

--- metplotpy/plots/base_plot.py
+++ metplotpy/plots/base_plot.py
@@ -33,7 +33,7 @@ from metplotpy.plots.context_filter import ContextFilter
 # kaleido.scope will be deprecated after September 2025
 # using plotly.io.defaults.* is recommended, but there is no way to add chromium args in this package.
 #pio.kaleido.scope.chromium_args += ("--single-process",)
-
+pio.install_chrome()
 class BasePlot:
     """A class that provides methods for building Plotly plot's common features
      like title, axis, legend.
