c3ca37b28a4c942bd91967532c631a3b048f99a0
bikegeek <minnawin@ucar.edu>
2025-07-10 08:26:27 -0600

    Incorporate update for kaleido 1.0.0/plotly 6.1.1 and checking for the PRE_LOAD_CHROME env, used to determine whether to retrieve Google Chrome at runtime

--- metplotpy/plots/base_plot.py
+++ metplotpy/plots/base_plot.py
@@ -20,15 +20,33 @@ import warnings
 import numpy as np
 import yaml
 from typing import Union
-
-import plotly.io as pio
+import kaleido
 
 import metplotpy.plots.util
 from .config import Config
 from metplotpy.plots.context_filter import ContextFilter
 
-# set kaleido to use single process to prevent GPU errors in containers
-pio.kaleido.scope.chromium_args += ("--single-process",)
+# kaleido 0.x will be deprecated after September 2025 and Chrome will no longer
+# be included with kaleido from version 1.0.0.  Explicitly get Chrome via call to kaleido.
+
+# In some instances, we do NOT want Chrome to be installed at run-time. If the 
+# PRE_LOAD_CHROME environment variable exists, or set to TRUE,
+# then Chrome will be assumed to have been pre-loaded. Otherwise,
+# invoke  get_chrome_sync()  to install Chrome in the
+# /path-to-python-libs/pythonx.yz/site-packages/...  directory
+
+# Check if the PRE_LOAD_CHROME env variable exists
+if 'PRE_LOAD_CHROME' not in os.environ:
+    kaleido.get_chrome_sync()
+else:
+    # Check value of PRE_LOAD_CHROME (could be boolean or string)
+    chrome_env = os.environ.get('PRE_LOAD_CHROME')
+    if type(chrome_env) is bool and chrome_env is False:
+         kaleido.get_chrome_sync()
+    else:
+        # ENV var is a string and has been set to 'False' (case-insensitive)
+        if chrome_env.lower() == 'false':
+            kaleido.get_chrome_sync()
 
 class BasePlot:
     """A class that provides methods for building Plotly plot's common features
