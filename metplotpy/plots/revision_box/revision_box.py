f0707c925803813722597efef82cec7371c81026
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2026-03-06 14:37:36 -0700

    fix incorrect location of caption by overriding _add_caption function to place annotation text at top left of plot regardless of config settings

--- metplotpy/plots/revision_box/revision_box.py
+++ metplotpy/plots/revision_box/revision_box.py
@@ -117,6 +117,20 @@ class RevisionBox(Box):
 
         self.logger.info(f"Begin creating the figure: {datetime.now()}")
 
+        self._create_annotation()
+
+        # set the x-axis labels to match the user legends
+        self.config_obj.indy_label = self.config_obj.user_legends
+
+        super()._create_figure()
+
+        self.logger.info(f"Finished creating figure: {datetime.now()}")
+
+    def _create_annotation(self):
+        if not self.config_obj.revision_run and not self.config_obj.revision_ac:
+            self.config_obj.plot_caption = None
+            return
+
         annotation_text_all = ''
         for inx, series in enumerate(self.series_list):
             # Don't generate the plot for this series if
@@ -124,7 +138,6 @@ class RevisionBox(Box):
             if not series.plot_disp:
                 continue
 
-            #self._draw_series(series)
             # construct annotation text
             annotation_text = series.user_legends + ': '
             if self.config_obj.revision_run:
@@ -137,16 +150,20 @@ class RevisionBox(Box):
 
             annotation_text_all = annotation_text_all + annotation_text
             if inx < len(self.series_list) - 1:
-                annotation_text_all = annotation_text_all + '<br>'
+                annotation_text_all = annotation_text_all + '\n'
 
         self.config_obj.plot_caption = annotation_text_all
 
-        # set the x-axis labels to match the user legends
-        self.config_obj.indy_label = self.config_obj.user_legends
-
-        super()._create_figure()
-
-        self.logger.info(f"Finished creating figure: {datetime.now()}")
+    def _add_caption(self, plt, font_properties):
+        """
+        Adds a caption to the top left of the plot, just below the title.
+        Always uses the same position regardless of the config file settings.
+        """
+        if self.config_obj.plot_caption:
+            plt.figtext(0.06, 0.90, self.config_obj.plot_caption,
+                        fontproperties=font_properties,
+                        color=self.config_obj.parameters['caption_col'],
+                        ha='left')
 
     def _add_custom_lines(self, ax):
         return
