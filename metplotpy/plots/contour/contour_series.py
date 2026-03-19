19b44f47131c45ffbd390f7fd04c2c442e2849aa
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2026-03-19 14:59:22 -0600

    Per #572, convert contour plot from plotly to matplotlib

--- metplotpy/plots/contour/contour_series.py
+++ metplotpy/plots/contour/contour_series.py
@@ -18,7 +18,7 @@ from datetime import datetime
 import numpy as np
 import warnings
 
-import metplotpy.plots.util_plotly as util
+import metplotpy.plots.util as util
 from ..series import Series
 
 
@@ -100,11 +100,11 @@ class ContourSeries(Series):
 
         self.logger.info(f"Creating the series points: {datetime.now()}")
         y_real = self.config.indy_vals.copy()
-        if self.config.reverse_x is True:
+        if self.config.xaxis_reverse:
             y_real.reverse()
 
         x_real = self.config.series_vals_1[0].copy()
-        if self.config.reverse_y is True:
+        if self.config.yaxis_reverse:
             x_real.reverse()
 
         z = [[None for i in range(len(y_real))] for j in range(len(x_real))]
